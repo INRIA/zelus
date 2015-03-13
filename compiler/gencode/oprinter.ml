a833c77ae07c591e7e46c4ec4b824e84a96437f0
Guillaume Baudart <guillaume.baudart@ens.fr>
2015-03-13 17:17:33 +0000

    bug type declaration polymorph record

--- compiler/gencode/oprinter.ml
+++ compiler/gencode/oprinter.ml
@@ -26,7 +26,7 @@ let priority e =
     | Oconst _ | Oconstr0 _| Oglobal _ | Olocal _ | Ostate _ | Oindex _
     | Orecord _ | Orecord_access _ | Otypeconstraint _ | Ofor _ | Owhile _
     | Otuple _ -> 3
-    | Oconstr1 _ | Oapp _ | Omethod _ | Oassign _ | Oassign_state _ -> 2 
+    | Oconstr1 _ | Oapp _ | Omethod _ | Oassign _ | Oassign_state _ -> 2
     | Olet _ | Oletvar _ | Oifthenelse _ | Omatch _ -> 1
     | Osequence _ -> 0
 
@@ -63,14 +63,14 @@ let longname ff ln =
 let name ff n = shortname ff (Ident.name n)
 
 let immediate ff = function
-  | Oint i -> 
+  | Oint i ->
       if i < 0 then fprintf ff "(%a)" pp_print_int i else pp_print_int ff i
-  | Oint32 i -> 
+  | Oint32 i ->
       if i < 0
       then fprintf ff "(%al)" pp_print_int i
       else fprintf ff "%al"   pp_print_int i
-  | Ofloat f -> 
-      if f < 0.0 then fprintf ff "(%a)" pp_print_float f 
+  | Ofloat f ->
+      if f < 0.0 then fprintf ff "(%a)" pp_print_float f
       else pp_print_float ff f
   | Obool b -> if b then fprintf ff "true" else fprintf ff "false"
   | Ostring s -> fprintf ff "%S" s
@@ -81,19 +81,19 @@ let is_otypearrow ty = match ty.desc with
   | Otypearrow _ -> true
   | _ -> false
 
-let kind = function 
-  | Deftypes.Tany | Deftypes.Tdiscrete _ -> "discrete" 
+let kind = function
+  | Deftypes.Tany | Deftypes.Tdiscrete _ -> "discrete"
   | Deftypes.Tcont -> "continuous"
 
 let rec ptype ff ty = match ty.desc with
-  | Otypevar(s) -> fprintf ff "%s" s
+  | Otypevar(s) -> fprintf ff "'%s" s
   | Otypearrow(ty1, ty2) when is_otypearrow ty1 ->
       fprintf ff "(%a) ->@ %a" ptype ty1 ptype ty2
   | Otypearrow(ty1, ty2) -> fprintf ff "%a ->@ %a" ptype ty1 ptype ty2
   | Otypetuple(ty_list) ->
       fprintf ff "@[<hov2>%a@]" (print_list_r ptype "("" *"")") ty_list
   | Otypeconstr(ln, ty_list) ->
-      fprintf ff "@[<hov2>%a@]%a" 
+      fprintf ff "@[<hov2>%a@]%a"
         (print_list_r_empty ptype "("","")") ty_list longname ln
   | Otypeobject(n_ty_list) ->
       fprintf ff "@[<hov2><%a>@]"
@@ -124,9 +124,9 @@ and pattern_comma_list ff pat_list =
   print_list_r pattern "("","")" ff pat_list
 
 and method_name m_name =
-  match m_name with 
-  | Ostep -> "step" | Oreset -> "reset" | Oderivatives -> "derivatives" 
-  | Ocrossings -> "crossings" | Omaxsize -> "maxsize" 
+  match m_name with
+  | Ostep -> "step" | Oreset -> "reset" | Oderivatives -> "derivatives"
+  | Ocrossings -> "crossings" | Omaxsize -> "maxsize"
   | Oreinit -> "reinit" | Ocin -> "cin" | Ocout -> "cout"
   | Ozin -> "zin" | Oclear_zin -> "clear_zin" | Ozout -> "zout" | Odout -> "dout"
   | Ocsize -> "csize" | Ozsize -> "zsize"
@@ -136,16 +136,16 @@ and method_call ff { c_machine = lname; c_method_name = m; c_instance = i_opt } 
   let m = method_name m in
   let instance ff i_opt =
     match i_opt with
-    | None -> (* a call to the self machine *) fprintf ff "self" 
+    | None -> (* a call to the self machine *) fprintf ff "self"
     | Some(o) -> fprintf ff "self.%a" name o in
   if !print_as_functions then fprintf ff "%a_%s %a" longname lname m instance i_opt
   else fprintf ff "%a.%s" instance i_opt m
 
-and reset lname ff o = 
-  if !print_as_functions 
+and reset lname ff o =
+  if !print_as_functions
   then fprintf ff "%a_reset self.%a" longname lname name o
   else fprintf ff "%a.reset()" name o
-  
+
 and left_value ff left =
   match left with
   | Oleft_name(n) -> name ff n
@@ -157,11 +157,11 @@ and left_value ff left =
 and left_state_value ff left =
   match left with
   | Oself -> fprintf ff "self."
-  | Oleft_instance_name(n) -> 
+  | Oleft_instance_name(n) ->
      if !print_as_functions then fprintf ff "self.%a" name n
      else name ff n
   | Oleft_state_global(ln) -> longname ff ln
-  | Oleft_state_name(n) -> 
+  | Oleft_state_name(n) ->
      if !print_as_functions then fprintf ff "self.%a" name n
      else name ff n
   | Oleft_state_record_access(left, n) ->
@@ -171,41 +171,41 @@ and left_state_value ff left =
   | Oleft_state_primitive_access(left, a) ->
      fprintf ff "@[%a.%a@]" left_state_value left access a
 
-and assign ff left e = 
-  let arrow = 
+and assign ff left e =
+  let arrow =
     match left with | Oleft_name _ -> ":=" | _ -> "<-" in
   fprintf ff "@[<v 2>%a %s %a@]" left_value left arrow (expression 2) e
 
-and assign_state ff left e = 
+and assign_state ff left e =
   fprintf ff "@[<v 2>%a <- %a@]" left_state_value left (expression 2) e
 
-and access ff a = 
-  let s = 
+and access ff a =
+  let s =
     match a with
     | Oderivative -> "der" | Ocontinuous -> "pos"
     | Ozero_out -> "zout"  | Ozero_in -> "zin" in
   fprintf ff "%s" s
 
-  
+
 and local ff n is_shared =
   fprintf ff "%s%a" (if is_shared then "!" else "") name n
 
 and letvar ff n ty e_opt e =
   match e_opt with
-    | None -> 
+    | None ->
         if !print_as_functions
-        then fprintf ff "@[<v 0>let %a = ref (Obj.magic (): %a) in@ %a@]" 
+        then fprintf ff "@[<v 0>let %a = ref (Obj.magic (): %a) in@ %a@]"
                 name n Ptypes.output ty (expression 0) e
-        else fprintf ff "@[<v 0>var %a: %a in@ %a@]" 
+        else fprintf ff "@[<v 0>var %a: %a in@ %a@]"
                 name n Ptypes.output ty (expression 0) e
-    | Some(e0) -> 
+    | Some(e0) ->
         if !print_as_functions
-        then fprintf ff "@[<v 0>let %a = ref (%a:%a) in@ %a@]" 
+        then fprintf ff "@[<v 0>let %a = ref (%a:%a) in@ %a@]"
           name n (expression 0) e0 Ptypes.output ty (expression 0) e
-        else fprintf ff "@[<v 0>var %a: %a = %a in@ %a@]" 
+        else fprintf ff "@[<v 0>var %a: %a = %a in@ %a@]"
           name n Ptypes.output ty (expression 0) e0 (expression 0) e
 
-and expression prio ff e = 
+and expression prio ff e =
   let prio_e = priority e in
   if prio_e < prio then fprintf ff "(";
   begin match e.desc with
@@ -224,7 +224,7 @@ and expression prio ff e =
   | Oapp(ln, []) ->
       fprintf ff "@[<hov2>%a ()@]" longname ln
   | Oapp(ln, e_list) ->
-      fprintf ff "@[<hov2>%a %a@]" 
+      fprintf ff "@[<hov2>%a %a@]"
         longname ln (print_list_r (expression 3) """""") e_list
   | Omethod(m, e_list) ->
       fprintf ff "@[<hov2>%a %a@]" method_call m
@@ -233,9 +233,9 @@ and expression prio ff e =
       print_record (print_couple longname (expression 4) """ =""") ff r
   | Orecord_access(e, lname) ->
       fprintf ff "%a.%a" (expression prio_e) e longname lname
-  | Otypeconstraint(e, ty_e) -> 
+  | Otypeconstraint(e, ty_e) ->
       fprintf ff "@[(%a : %a)@]" (expression prio_e) e ptype ty_e
-  | Olet(pat_exp_list, e) -> 
+  | Olet(pat_exp_list, e) ->
       fprintf ff "@[<v 0>%a@ %a@]"
         (print_list_l pat_exp "let ""and "" in") pat_exp_list
         (expression (prio_e-1)) e
@@ -249,7 +249,7 @@ and expression prio ff e =
         (expression 0) e (expression prio_e) e1 (expression prio_e) e2
   | Ofor(is_to, n, e1, e2, e3) ->
       fprintf ff "@[<hv>for %a = %a %s %a@ @[<hv 2>do@ %a@ done@]@]"
-        name n (expression 0) e1 (if is_to then "to" else "downto") 
+        name n (expression 0) e1 (if is_to then "to" else "downto")
         (expression 0) e2 (expression 0) e3
   | Owhile(e1, e2) ->
       fprintf ff "@[<hv>while %a do %a done@]@]"
@@ -257,12 +257,12 @@ and expression prio ff e =
   | Oassign(left, e) -> assign ff left e
   | Oassign_state(left, e) -> assign_state ff left e
   | Osequence(e1, e2) ->
-      fprintf ff "@[<hv>@[%a@];@ @[%a@]@]" 
-	      (expression (prio_e+2)) e1 (expression prio_e) e2
+      fprintf ff "@[<hv>@[%a@];@ @[%a@]@]"
+              (expression (prio_e+2)) e1 (expression prio_e) e2
   end;
   if prio_e < prio then fprintf ff ")"
 
-and pat_exp ff (p, e) = 
+and pat_exp ff (p, e) =
   fprintf ff "@[@[%a@] =@ @[%a@]@]" pattern p (expression 0) e
 
 and match_handler ff { w_pat = pat; w_body = b } =
@@ -285,111 +285,111 @@ and constr_decl ff = function
 
 let print_memory ff (n, (m, ty, e_opt)) =
   let mem ff mem =
-    match mem with 
-    | Discrete -> fprintf ff "disc" 
+    match mem with
+    | Discrete -> fprintf ff "disc"
     | Zero -> fprintf ff "zero"
-    | Cont -> fprintf ff "cont" in 
+    | Cont -> fprintf ff "cont" in
   match e_opt with
   | None -> fprintf ff "%a %a : %a" mem m name n Ptypes.output ty
-  | Some(e) -> 
+  | Some(e) ->
        fprintf ff "%a %a : %a = %a" mem m name n Ptypes.output ty expression e
 
-let print_name_with_type ff (n, ty) = 
+let print_name_with_type ff (n, ty) =
   fprintf ff "%a : %a" name n Ptypes.output ty
 
-let one_instance ff (n, ln, k) = 
+let one_instance ff (n, ln, k) =
   fprintf ff "@[%a : %s(%a)@]" name n (kind k) longname ln
-        
+
 let one_method ff { m_name = m_name; m_param = p_list; m_body = e } =
   fprintf ff "@[<hov 2>method %s %a =@ %a@]"
     (method_name m_name) pattern_list p_list expression e
 
 (** Define the data-type for the internal state of a machine *)
 let def_type_for_a_machine ff f memories instances =
-  let one_entry ff (n, m) = 
+  let one_entry ff (n, m) =
     fprintf ff "@[mutable %a : '%s@]" name n m in
-  let i, params, entries = 
+  let i, params, entries =
     List.fold_right
-      (fun (n, _) (i, params, entries) -> 
+      (fun (n, _) (i, params, entries) ->
         let m = Misc.int_to_alpha i in (i+1, m :: params, (n, m) :: entries))
       memories (0, [], []) in
-  let i, params, entries = 
+  let i, params, entries =
     List.fold_right
-      (fun (n, _, _) (i, params, entries) -> 
+      (fun (n, _, _) (i, params, entries) ->
         let m = Misc.int_to_alpha i in (i+1, m :: params, (n, m) :: entries))
       instances (i, params, entries) in
   (* if the state is empty, produce the dummy state type [unit] *)
   if entries = []
   then fprintf ff "@[type %s = unit@.@.@]" f
-  else 
+  else
       fprintf ff "@[<v 2>type @[%a@] %s =@ { @[%a@] }@.@.@]"
         (print_list_r (fun ff s -> fprintf ff "'%s" s) "("","")") params
         f
         (print_list_r one_entry """;""") entries
 
 let def_cont_type ff =
-  fprintf ff 
-	  "@[type continuous = { mutable pos: float; mutable der: float }@.@]"
+  fprintf ff
+          "@[type continuous = { mutable pos: float; mutable der: float }@.@]"
 
 let def_zero_crossing_type ff =
-  fprintf ff 
-	  "@[type zerocrossing = { mutable zin: bool; mutable zout: float }@.@]"
+  fprintf ff
+          "@[type zerocrossing = { mutable zin: bool; mutable zout: float }@.@]"
 
 let def_signal_type ff =
-  fprintf ff 
-	  "@[type 'a signal = 'a * bool@.\
+  fprintf ff
+          "@[type 'a signal = 'a * bool@.\
              type zero = bool@.@]"
 
 (** Define the method *)
-let def_method_as_a_function f ff 
+let def_method_as_a_function f ff
     { m_name = m_name; m_param = pat_list; m_body = e } =
-  fprintf ff "@[<v 2>let %s_%s self %a =@ %a@.@.@]" 
+  fprintf ff "@[<v 2>let %s_%s self %a =@ %a@.@.@]"
     f (method_name m_name) pattern_list pat_list expression e
-   
+
 (** Define the method *)
 let def_method_as_a_function_name f ff { m_name = m_name } =
   fprintf ff "%s_%s" f (method_name m_name)
- 
+
 (** Define the allocation function *)
 let def_alloc_for_a_machine ff f memories instances =
-  let one_memory ff (n, (m, ty, e_opt)) = 
+  let one_memory ff (n, (m, ty, e_opt)) =
     match m with
     | Zero -> fprintf ff "%a = { zin = false; zout = 1.0 }" name n
     | Cont ->
       begin match e_opt with
-	| None -> fprintf ff "%a = { pos = 0.0; der = 0.0 }" name n
-	| Some(e) -> fprintf ff "%a = { pos = %a; der = 0.0 }" name n expression e
+        | None -> fprintf ff "%a = { pos = 0.0; der = 0.0 }" name n
+        | Some(e) -> fprintf ff "%a = { pos = %a; der = 0.0 }" name n expression e
       end
     | Discrete ->
       begin match e_opt with
-	| None -> 
-	  fprintf ff "@[%a = (Obj.magic (): %a)@]" name n Ptypes.output ty
-	| Some(e) -> 
-	  fprintf ff "@[%a = (%a: %a)@]" name n expression e Ptypes.output ty
-	end in
-  let one_instance ff (n, ln, k) = 
+        | None ->
+          fprintf ff "@[%a = (Obj.magic (): %a)@]" name n Ptypes.output ty
+        | Some(e) ->
+          fprintf ff "@[%a = (%a: %a)@]" name n expression e Ptypes.output ty
+        end in
+  let one_instance ff (n, ln, k) =
     fprintf ff "@[%a = %a_alloc () (* %s *) @]" name n longname ln (kind k)  in
-  if memories = [] 
+  if memories = []
   then if instances = []
        then fprintf ff "@[let %s_alloc () = ()@.@.@]" f
-       else 
-	 fprintf ff "@[<v 2>let %s_alloc () =@ { @[%a@] }@.@.@]"
-		 f (print_list_r one_instance """;""") instances
+       else
+         fprintf ff "@[<v 2>let %s_alloc () =@ { @[%a@] }@.@.@]"
+                 f (print_list_r one_instance """;""") instances
   else if instances = []
   then
     fprintf ff "@[<v 2>let %s_alloc () =@ { @[%a@] }@.@.@]"
-	    f (print_list_r one_memory """;""") memories
+            f (print_list_r one_memory """;""") memories
   else
     fprintf ff "@[<v 2>let %s_alloc () =@ { @[%a%a@] }@.@.@]"
-	    f
-	    (print_list_r one_memory """;"";") memories
-	    (print_list_r one_instance """;""") instances
-  
+            f
+            (print_list_r one_memory """;"";") memories
+            (print_list_r one_instance """;""") instances
+
 (** Print a machine *)
 let machine_as_a_class ff f { m_kind = k;
-			      m_memories = memories;
-                              m_instances = instances; 
-			      m_methods = m_list } = 
+                              m_memories = memories;
+                              m_instances = instances;
+                              m_methods = m_list } =
   fprintf ff "@[<hov 2>let %s = machine(%s) {@,\
                   @[@[<v 2>memories @[%a@]@]@;\
                   @[@[<v 2>instances @[%a@]@]@;\
@@ -404,37 +404,37 @@ let machine_as_a_class ff f { m_kind = k;
 (* order methods in this order: *)
 (* alloc < maxsize < derivatives < step < crossings < reset *)
 let order m1 m2 =
-  let int { m_name = m_name } = 
-    match m_name with 
+  let int { m_name = m_name } =
+    match m_name with
       | Omaxsize -> 1 | Oderivatives -> 2
-      | Ostep -> 3 | Ocrossings -> 4 | Oreset -> 5 
-      | Ocin -> 6 | Ocout -> 7 | Odout -> 8 
+      | Ostep -> 3 | Ocrossings -> 4 | Oreset -> 5
+      | Ocin -> 6 | Ocout -> 7 | Odout -> 8
       | Ozin -> 9 | Ozout -> 10 | _ -> 0 in
   Pervasives.compare (int m1) (int m2)
 
-let tuple ff f k m_list = 
+let tuple ff f k m_list =
   match k with
     | Deftypes.Tany -> ()
     | Deftypes.Tdiscrete _ | Deftypes.Tcont ->
         let m_list = List.sort order m_list in
-	fprintf ff
-	  "@[<hov 2>let %s = %s_alloc, %a@.@]" f f
-	  (print_list_r (def_method_as_a_function_name f) """,""") m_list
+        fprintf ff
+          "@[<hov 2>let %s = %s_alloc, %a@.@]" f f
+          (print_list_r (def_method_as_a_function_name f) """,""") m_list
 
 (** Print a machine as pieces with a type definition for the state *)
 (** an a collection of functions *)
 let machine_as_functions ff f { m_kind = k;
-				m_memories = memories;
-                                m_instances = instances; 
-				m_methods = m_list } = 
-  def_type_for_a_machine 
+                                m_memories = memories;
+                                m_instances = instances;
+                                m_methods = m_list } =
+  def_type_for_a_machine
     ff f memories instances;
-  def_alloc_for_a_machine 
+  def_alloc_for_a_machine
     ff f memories instances;
   (print_list_r (def_method_as_a_function f) """""") ff m_list;
   tuple ff f k m_list
 
-let machine ff f m = 
+let machine ff f m =
   if !print_as_functions then machine_as_functions ff f m
   else machine_as_a_class ff f m
 
@@ -443,7 +443,7 @@ let implementation ff impl = match impl.desc with
   | Oletvalue(n, e) ->
       fprintf ff "@[<v 2>let %a = %a@.@.@]" shortname n expression e
   | Oletfun(n, pat_list, e) ->
-      fprintf ff "@[<v 2>let %a %a =@ %a@.@.@]" 
+      fprintf ff "@[<v 2>let %a %a =@ %a@.@.@]"
         shortname n pattern_list pat_list expression e
   | Oletmachine(n, m) -> machine ff n m
   | Oopen(s) ->
@@ -458,9 +458,9 @@ let implementation ff impl = match impl.desc with
             "type ""and """)
         l
 
-let implementation_list ff as_functions impl_list = 
+let implementation_list ff as_functions impl_list =
   print_as_functions := as_functions;
   fprintf ff "@[(* %s *)@.@]" header_in_file;
-  if !print_as_functions 
+  if !print_as_functions
   then begin def_cont_type ff; def_zero_crossing_type ff; def_signal_type ff end;
   List.iter (implementation ff) impl_list
