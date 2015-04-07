44e81e841a3986ebdc7e4ccba0aa30a0ecb8f335
Guillaume Baudart <guillaume.baudart@ens.fr>
2015-04-07 14:23:52 +0000

    cleanning ltta.zls

--- examples/ltta/misc.ml
+++ examples/ltta/misc.ml
@@ -1,11 +1,15 @@
 (* Random generator *)
-let rand_val min max =
-  (Random.float (max -. min)) +. min
+let arbitrary min max =
+  (max +. min) /. 2.
+  (* (Random.float (max -. min)) +. min *)
 
 (* Queue API *)
 let empty () = []
-let front l = print_endline "pop"; List.hd (List.rev l)
-let dequeue l = print_endline "tail";  List.tl l
-let enqueue l i = print_endline "push"; i::l
+let front l = List.hd (List.rev l)
+let dequeue l = List.tl l
+let enqueue l i = i::l
 let sum l = List.fold_left (+.) 0.0 l
 let is_empty l = (l == [])
+
+(* Print *)
+let print o1 o2 = Format.printf "o1 = %d, o2 = %d\n" o1 o2
--- examples/ltta/misc.mli
+++ examples/ltta/misc.mli
@@ -1,7 +1,8 @@
-val rand_val : float -> float -> float
+val arbitrary : float -> float -> float
 val empty : unit -> 'a list
 val front : 'a list -> 'a
 val dequeue : 'a list -> 'a list
 val enqueue : 'a list -> 'a -> 'a list
 val sum : float list -> float
 val is_empty : 'a list -> bool
+val print: int -> int -> unit
