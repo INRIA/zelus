d015e400d946974a43cd660e9af3cb3930d2fdf4
Timothy Bourke <tim@tbrk.org>
2017-10-19 17:30:16 +0200

    Tune horloge example interfaces

--- examples/horloge/draw.mli
+++ examples/horloge/draw.mli
@@ -1,15 +1,15 @@
 val draw_system :
-  float ->
-  float ->
-  float ->
-  float ->
-  float ->
-  float ->
-  float ->
-  float ->
-  float ->
-  int ->
-  int ->
+  float *
+  float *
+  float *
+  float *
+  float *
+  float *
+  float *
+  float *
+  float *
+  int *
+  int *
   int -> unit
 val input : unit -> bool
 val play_tic : unit -> unit
