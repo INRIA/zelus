17e708b383cc8907bb615451793274c20b329c75
Marc Pouzet <you@example.com>
2018-08-29 17:03:52 +0200

    The pendulum can now be freezed, started, resumed.

--- examples/pendulum/aux.ml
+++ examples/pendulum/aux.ml
@@ -12,3 +12,29 @@ let input () =
   | Some 'q' -> exit 0
   | Some 'r' -> Resume
   | _ -> Nothing
+
+(* an other version *)
+(*
+let read_key () =
+  let r = ref  in
+  ignore (Thread.create (fun () -> r := Some(Graphics.read_key ())) ());
+  r
+
+(* convert an option type into a Zelus signal. This is ugly *)
+(* as it use the Obj.magic function. This is because signals *)
+(* are not implemented as option type, as it was in LS, but as *)
+(* a dependent pair. This is temporary, as long as sum types are not *)
+(* provided in Zelus *)
+let get s =
+  (match !s with
+  | None -> false, Obj.magic ()
+  | Some(v) -> true, v
+
+(* Then, this function would not block *)
+let node input () =
+  reset 
+    init s = Aux.read_key ()  (* s est une promesse *)
+   and
+    present Aux.get(s)(v) -> emit c = v
+  every false -> ?last c
+*)
