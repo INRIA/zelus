e41c88f83a6196b48e7d602acc76e77f35d4b4eb
Louis Mandel <lmandel@us.ibm.com>
2019-01-09 09:57:55 -0500

    Update display in HMM example.

--- probabilistic/examples/hmm/display.ml
+++ probabilistic/examples/hmm/display.ml
@@ -24,15 +24,25 @@ let draw_point_dist dist =
   begin match dist with
   | Dist_sampler (_, _) -> assert false
   | Dist_support support ->
-      List.iter
-        (fun (pos, prob) ->
-           let color =
-             let n = int_of_float (255. *. (1. -. max 0.4 prob)) in
-             if prob > 0.003 then Graphics.black
-             else Graphics.rgb n n n
-           in
-           draw_point color pos
-        ) support
+      (* List.iter *)
+      (*   (fun (pos, prob) -> *)
+      (*      let color = *)
+      (*        let n = int_of_float (255. *. (1. -. max 0.4 prob)) in *)
+      (*        if prob > 0.003 then Graphics.black *)
+      (*        else Graphics.rgb n n n *)
+      (*      in *)
+      (*      draw_point color pos *)
+      (*   ) support *)
+      let support =
+        List.sort (fun (_, prob1) (_, prob2) -> compare prob1 prob2) support
+      in
+      let len = 1 + (List.length support / 200) in
+      let color = ref (List.length support / len + 1)  in
+      List.iteri
+        (fun i (pos, prob) ->
+           if i mod len = 0 then decr color;
+           draw_point (Graphics.rgb !color !color !color) pos)
+        support
   end
 
 let () = Random.self_init()
