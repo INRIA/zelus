b8d956e7ed2cf39ff4a138911845945e451a3a17
Guillaume Baudart <guillaume.baudart@ibm.com>
2019-01-09 15:06:05 -0500

    Important commit

--- probabilistic/inference/infer_importance.ml
+++ probabilistic/inference/infer_importance.ml
@@ -1,13 +1,42 @@
 open Ztypes
+open Infer
 
 type pstate = Infer.pstate
 
 let sample = Infer.sample
-
 let factor = Infer.factor
 
-let infer n node =
-  let Node { alloc; reset; step } = Infer.infer n node in
-  Node { alloc = alloc;
-         reset = reset;
-         step = (fun state input -> step state (false, input)); }
+
+
+let infer n (Node { alloc; reset; step }) =
+  let alloc () =
+    { infer_states = Array.init n (fun _ -> alloc ());
+      infer_scores = Array.make n 0.0; }
+  in
+  let reset state =
+    Array.iter reset state.infer_states;
+    Array.fill state.infer_scores 0 n 0.0
+  in
+  let step { infer_states = states; infer_scores = scores } input =
+    let values =
+      Array.mapi
+        (fun i state ->
+          let value = step state ({ idx = i; scores = scores; }, input) in
+          value)
+        states
+    in
+    let weights, norm =
+      let sum = ref 0. in
+      let acc = ref [] in
+      Array.iteri
+        (fun i score ->
+          let w = max (exp score) epsilon_float in
+          acc := (values.(i), w) :: !acc;
+          sum := !sum +. w)
+        scores;
+      (!acc, !sum)
+    in
+    Distribution.Dist_support
+      (List.map (fun (b, w) -> (b, w /. norm)) weights)
+  in
+  Node { alloc = alloc; reset = reset; step = step }
