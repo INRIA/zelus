8c81941ab29197cb658ccc3ca5f0177e9a730f80
Guillaume Baudart <guillaume.baudart@ibm.com>
2019-01-18 17:03:21 -0500

    add manual controller

--- probabilistic/examples/slam/array_misc.ml
+++ probabilistic/examples/slam/array_misc.ml
@@ -30,23 +30,40 @@ let print_map_dist a =
     (Distribution.split_array a)
 
 
-let () =
-  Graphics.open_graph " 550x100";
-  Graphics.auto_synchronize false
+let init_graph max_pos =
+  let size = " "^(string_of_int ((max_pos + 1) * 50))^"x100" in
+  Graphics.open_graph size;
+  Graphics.auto_synchronize false;
+  Format.printf "Press in the graphic window:@.";
+  Format.printf "- 'l' to go left@.";
+  Format.printf "- 'r' to go right@.";
+  Format.printf "- 'q' to quit@.";
+  Format.printf "- any other key for automatic control@."
 
 let clear () =
   Graphics.synchronize ();
   Graphics.clear_graph ()
 
 let wait_event () =
-  ignore (Graphics.read_key ())
+  let c = Graphics.read_key () in
+  begin match c with
+  | 'l' ->  -1
+  | 'r' ->  1
+  | 'q' -> exit 0
+  | _ -> 0
+  end
+  (* ignore (Graphics.read_key ()) *)
 
 let width = 50
-let height = 100
+let height = 50
 
-let draw_position x =
+let draw_bot x obs =
   Graphics.set_color (Graphics.blue);
-  Graphics.fill_circle (x * width + width / 2) (3 * height / 4) 10
+  Graphics.fill_circle (x * width + width / 2) (3 * height / 2) 10;
+  if obs then Graphics.set_color (Graphics.white)
+  else Graphics.set_color (Graphics.black);
+  Graphics.fill_circle (x * width + width / 2) (3 * height / 2) 5
+  
 
 let draw_position_dist d =
   match d with
@@ -56,7 +73,7 @@ let draw_position_dist d =
         (fun (x, p) ->
            Graphics.set_color (Graphics.red);
            Graphics.fill_circle
-             (x * width + width / 2) (height / 4)
+             (x * width + width / 2) (height / 2)
              (1 + int_of_float (10. *. p)))
         support
 	
@@ -73,7 +90,14 @@ let draw_map_dist map_dist =
     Graphics.set_color (Graphics.rgb gray gray gray);
     Graphics.fill_rect (i * width)  0  width height)
     mw
-      
+
+let draw_map m =
+  Array.iteri (fun i b ->
+    if b then Graphics.set_color (Graphics.white)
+    else Graphics.set_color (Graphics.black);
+    Graphics.fill_rect (i * width)  height  width height)
+    m
+    
 let random n theta =
   Array.init n
     (fun _ -> Distribution.draw (Distribution.bernoulli theta))
