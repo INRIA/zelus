267fe8008a53b8952ace0db500a77c977cbe2f79
ismailbennani <ismail.lahkim.bennani@ens.fr>
2019-02-15 12:45:33 +0100

    added spec for bangbang

---
+++ examples/bangbang/bangbang_main.ml
@@ -0,0 +1,15 @@
+open Ztypes
+open Zls
+(* simulation (continuous) function *)
+let main = let Hybrid { alloc = alloc; step = hstep; reset = reset } = Bangbang.main in 
+           let step mem c d zin t = discrete := true; cvec := c;dvec := d; zinvec := zin;cindex := 0; zindex := 0;horizon := infinity; hstep mem (t, ()) in
+           let derivative mem c d zin zout t = discrete :=false; cvec := c; dvec := d;zinvec := zin; zoutvec := zout;cindex := 0; zindex := 0;hstep mem (t, ()) in
+           let crossings mem c zin zout t =  discrete := false; cvec := c;zinvec := zin; zoutvec := zout;cindex := 0; zindex := 0;hstep mem (t, ()) in
+           let maxsize mem = (!cmax, !zmax) in
+           let csize mem = !cindex in
+           let zsize mem = !zindex in
+           let horizon mem = !horizon in
+           Hsim {alloc; step; reset; derivative; crossings; maxsize; csize; zsize; horizon};;
+(* instantiate a numeric solver *)
+module Runtime = Zlsrungtk.Make (Defaultsolver)
+let _ = Runtime.go main
