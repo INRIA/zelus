d8bb3d1ea6a3bfc1e36caa32eab519ec3f1c434a
Louis Mandel <lmandel@us.ibm.com>
2019-02-20 16:58:33 -0500

    Use hashtable to build histogram.

--- probabilistic/inference/normalize.ml
+++ probabilistic/inference/normalize.ml
@@ -2,19 +2,24 @@
 let copy : 'a. 'a -> 'a =
   fun x -> Marshal.from_bytes (Marshal.to_bytes x [Marshal.Closures]) 0
 
+
+(** [histogram_of_array l] create a histogram from an array of values. *)
+let histogram_of_array l =
+  let tbl = Hashtbl.create 7 in
+  Array.iter
+    (fun x ->
+       try
+         let n = Hashtbl.find tbl x in
+         Hashtbl.replace tbl x (n + 1)
+       with Not_found -> Hashtbl.add tbl x 1)
+    l;
+  Hashtbl.fold (fun x n acc -> (x, n)::acc) tbl []
+
 (** [normalize values] creates a distribution corresponding to the
     array of values [values]. *)
 let normalize values =
   let norm = float (Array.length values) in
-  let return_histogram =
-    Array.fold_left
-      (fun acc v ->
-         Misc_lib.list_replace_assoc v
-           (function None -> 1
-                   | Some n -> n + 1)
-           acc)
-      [] values
-  in
+  let return_histogram = histogram_of_array values in
   Distribution.Dist_support
     (List.map (fun (v, n) -> (v, float n /. norm)) return_histogram)
 
@@ -32,10 +37,11 @@ let resample (states, scores, values) =
       scores;
     (!acc, !sum)
   in
-  let dist =
-    Distribution.Dist_support
-      (List.map (fun (b, w) -> (b, w /. norm)) weights)
+  let support =
+    List.sort (fun (_, x) (_, y) -> compare y x)
+      (List.rev_map (fun (b, w) -> (b, w /. norm)) weights)
   in
+  let dist = Distribution.Dist_support support in
   Array.iteri
     (fun i _ ->
        let state, value = copy (Distribution.draw dist) in
