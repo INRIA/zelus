c217d6b1bb5cbe8a97e1164b8ae1b043b17e7bfb
Louis Mandel <lmandel@us.ibm.com>
2019-03-15 19:23:17 -0400

    Code cleanup.

--- probabilistic/inference/infer_ds.ml
+++ probabilistic/inference/infer_ds.ml
@@ -4,21 +4,23 @@ type pstate = {
   mutable score : float;
 }
 
-let factor (prob, f) =
-  prob.score <- prob.score +. f
-
 type mgaussiant = float
+type mbetat = float
+type mbernoullit = bool
 type 'a mtype = 'a
 
+(** Marginalized distribution *)
 type 'a mdistr =
   | MGaussian : float * float -> mgaussiant mdistr
-  (* MBeta :: !Double -> !Double -> MDistr MBetaT *)
+  | MBeta : float * float -> mbetat mdistr
   | MBernoulli : float -> bool mdistr
 
+(** Conditionned distribution *)
 type ('m1, 'm2) cdistr =
   | AffineMeanGaussian: float * float * float -> (mgaussiant, mgaussiant) cdistr
-  (* CBernoulli :: CDistr MBetaT MBernoulliT *)
+  | CBernoulli : (mbetat, mbernoullit) cdistr
 
+(** Node state *)
 type 'a state =
   | Initialized
   | Marginalized of 'a mdistr
@@ -40,27 +42,73 @@ and 'b node_from =
 
 and ('a, 'b) node' = ('a, 'b) node
 
-let recip x = 1. /. x
+
+let factor (prob, f) =
+  prob.score <- prob.score +. f
+
+let mdistr_to_distr (type a): a mdistr -> a Distribution.t = fun mdistr ->
+  begin match mdistr with
+    | MGaussian (mu, var) -> Distribution.gaussian mu (sqrt var)
+    | MBeta (alpha, beta) -> Distribution.beta alpha beta
+    | MBernoulli p -> Distribution.bernoulli p
+  end
+
+let cdistr_to_mdistr (type m) (type m'): (m, m') cdistr -> m mtype -> m' mdistr =
+  fun cdistr obs ->
+  begin match cdistr with
+    | AffineMeanGaussian (m, b, obsvar) ->
+        MGaussian (m *. obs +. b, obsvar)
+    | CBernoulli ->
+        MBernoulli obs
+  end
+
 
 let mgaussian mu var = MGaussian (mu, var)
-let affine_mean_gaussian m b var = AffineMeanGaussian (m, b, var)
+let mbeta alpha beta = MBeta (alpha, beta)
+let mbernoulli theta = MBernoulli theta
 
-(* gaussianConditioning :: Double -> Double -> Double -> Double -> (Double, Double) *)
-let gaussian_conditioning mu var obs obsvar =
-  let ivar = recip var in
-  let iobsvar = recip obsvar in
-  let inf = ivar +. iobsvar in
-  let var' = recip inf in
-  let mu' = (ivar *. mu +. iobsvar *. obs) /. inf in
-  (mu', var')
+let affine_mean_gaussian m b var = AffineMeanGaussian (m, b, var)
 
 let gaussian_mean_gaussian: float -> (mgaussiant, mgaussiant) cdistr =
   fun x ->
   AffineMeanGaussian (1., 0., x)
 
+let make_marginal (type a) (type b): a mdistr -> (a, b) cdistr -> b mdistr =
+  fun mdistr cdistr ->
+  begin match mdistr, cdistr with
+    | MGaussian (mu, var), AffineMeanGaussian(m, b, obsvar) ->
+        MGaussian (m *. mu +. b, m ** 2. *. var +. obsvar)
+    | MBeta (a, b),  CBernoulli ->
+        MBernoulli (a /. (a +. b))
+    | _ -> assert false (* error "impossible" *)
+  end
+
+let make_conditional (type a) (type b):
+  a mdistr -> (a, b) cdistr -> b mtype -> a mdistr =
+  let gaussian_conditioning mu var obs obsvar =
+    let ivar = 1. /. var in
+    let iobsvar = 1. /. obsvar in
+    let inf = ivar +. iobsvar in
+    let var' = 1. /. inf in
+    let mu' = (ivar *. mu +. iobsvar *. obs) /. inf in
+    (mu', var')
+  in
+  fun mdistr cdistr obs ->
+  begin match mdistr, cdistr with
+    | MGaussian(mu, var), AffineMeanGaussian(m, b, obsvar) ->
+        let (mu', var') =
+          gaussian_conditioning mu var ((obs -. b) /. m) (obsvar /. m ** 2.)
+        in
+        MGaussian(mu', var')
+    | MBeta (a, b),  CBernoulli ->
+        if obs then MBeta (a +. 1., b) else MBeta (a, b +. 1.)
+    | _, _ -> assert false (* error "impossible" *)
+  end
+
+
 (* initialize without parent node *)
-(* constant' :: String -> MDistr a -> Node z a *)
-let assume_constant n d =
+let assume_constant (type a) (type z): string -> a mdistr -> (z, a) node =
+  fun n d ->
   { name = n;
     children = [];
     state = Marginalized d;
@@ -68,7 +116,9 @@ let assume_constant n d =
 
 (* initialize with parent node *)
 (* newConditional' :: String -> IORef (Node a b) -> CDistr b c -> IO (IORef (Node b c)) *)
-let assume_conditional str par cdistr =
+let assume_conditional (type a) (type b) (type c):
+  string -> (a,b) node -> (b, c) cdistr -> (b, c) node =
+  fun str par cdistr ->
   let child =
     { name = str
     ; children = []
@@ -79,60 +129,24 @@ let assume_conditional str par cdistr =
   child
 
 
-let cdistr_to_distr (type m) (type m'): (m, m') cdistr -> m mtype -> m' mdistr =
-  fun cdistr obs ->
-  begin match cdistr with
-    | AffineMeanGaussian (m, b, obsvar) ->
-        MGaussian (m *. obs +. b, obsvar)
-  end
-
-let make_marginal (type a) (type b): a mdistr -> (a, b) cdistr -> b mdistr =
-  fun mdistr cdistr ->
-  begin match mdistr, cdistr with
-    | MGaussian (mu, var), AffineMeanGaussian(m, b, obsvar) ->
-        MGaussian (m *. mu +. b, m ** 2. *. var +. obsvar)
-    | MBernoulli _, _ -> assert false (* error "impossible" *)
-  end
-
-let make_conditional (type a) (type b):
-  a mdistr -> (a, b) cdistr -> b mtype  -> a mdistr =
-  fun mdistr cdistr obs ->
-  begin match mdistr, cdistr with
-    | MGaussian(mu, var), AffineMeanGaussian(m, b, obsvar) ->
-        let (mu', var') =
-          gaussian_conditioning mu var ((obs -. b) /. m) (obsvar /. m ** 2.)
-        in
-        MGaussian(mu', var')
-    | MBernoulli _, _ -> assert false (* error "impossible" *)
-  end
-
-
 let marginalize (type a) (type b): (a, b) node -> unit =
   fun n ->
-  (* writeLog ("marginalize' " ++ name n) *)
   (* Format.printf "marginalize: %s@." n.name; *)
   begin match n.state, n.distr with
-  | Initialized, CDistr (par, cdistr) ->
-      let marg =
-       begin match par.state with
-         | Realized x ->
-             cdistr_to_distr cdistr x
-         | Marginalized parMarginal ->
-             make_marginal parMarginal cdistr
-         | Initialized -> assert false (* error "marginalize'" *)
-       end
-      in
-      (* Format.printf "marginalize: %s is marginalized!!!!@." n.name; *)
-      n.state <- Marginalized marg
-  | state, _ ->
-      Format.eprintf "Error: marginalize: %s@." n.name;
-      assert false (* error $  "marginalize': " ++ name n *)
-  end
-
-let mdistr_to_distr (type a): a mdistr -> a Distribution.t = fun mdistr ->
-  begin match mdistr with
-    | MGaussian (mu, var) -> Distribution.gaussian mu (sqrt var)
-    | MBernoulli p -> Distribution.bernoulli p
+    | Initialized, CDistr (par, cdistr) ->
+        let marg =
+          begin match par.state with
+            | Realized x ->
+                cdistr_to_mdistr cdistr x
+            | Marginalized parMarginal ->
+                make_marginal parMarginal cdistr
+            | Initialized -> assert false (* error "marginalize'" *)
+          end
+        in
+        n.state <- Marginalized marg
+    | state, _ ->
+        Format.eprintf "Error: marginalize %s@." n.name;
+        assert false
   end
 
 let rec delete n l =
@@ -143,11 +157,9 @@ let rec delete n l =
         else NodeFrom x :: (delete n l)
   end
 
-(* realize :: MType b -> IORef (Node a b) -> IO () *)
 let realize (type a) (type b): b mtype -> (a, b) node -> unit =
   fun val_ n ->
   (* Format.printf "realize: %s@." n.name; *)
-  (* writeLog ("realize " ++ name n) *)
   (* ioAssert (isTerminal n) *)
   begin match n.distr with
     | UDistr _ -> ()
@@ -163,12 +175,10 @@ let realize (type a) (type b): b mtype -> (a, b) node -> unit =
   n.state <- Realized val_;
   n.children <- []
 
-(* sample :: IORef (Node a b) -> IO () *)
 let sample (type a) (type b) : (a, b) node -> unit =
   fun n ->
   (* Format.printf "sample: %s@." n.name; *)
   (* ioAssert (isTerminal n) *)
-  (* writeLog ("sample " ++ name n) *)
   begin match n.state with
     | Marginalized m ->
         let x = Distribution.draw (mdistr_to_distr m) in
@@ -176,16 +186,14 @@ let sample (type a) (type b) : (a, b) node -> unit =
     | _ -> assert false (* error "sample" *)
   end
 
-
-(* observe :: MType b -> IORef (Node a b) -> M () *)
-let observe (prob, x, n) =
+let observe (type a) (type b): pstate -> b mtype -> (a, b) node -> unit =
+  fun prob x n ->
   (* io $ ioAssert (isTerminal n) *)
-  (* io $ writeLog ("observe " ++ name n) *)
   begin match n.state with
-  | Marginalized marg ->
-      factor (prob, Distribution.score (mdistr_to_distr marg) x);
-      realize x n
-  | _ -> assert false (* error "observe'" *)
+    | Marginalized marg ->
+        factor (prob, Distribution.score (mdistr_to_distr marg) x);
+        realize x n
+    | _ -> assert false (* error "observe'" *)
   end
 
 let is_marginalized state =
@@ -195,17 +203,14 @@ let is_marginalized state =
   end
 
 (* Invariant 2: A node always has at most one marginal Child *)
-(* marginalChild :: Node a b -> IO (Maybe (RefNodeFrom b)) *)
-let marginal_child n =
+let marginal_child (type a) (type b): (a, b) node -> b node_from option =
+  fun n ->
   List.find_opt
     (fun (NodeFrom x) -> is_marginalized x.state)
     n.children
 
-(* (\* prune :: IORef (Node a b) -> IO () *\) *)
 let rec prune : 'a 'b. ('a, 'b) node -> unit = function n ->
   (* Format.printf "prune: %s@." n.name; *)
-  (* n <- readIORef nref *)
-  (* writeLog ("prune " ++ name n) *)
   (* assert (isMarginalized (state n)) $ do *)
   begin match marginal_child n with
     | Some (NodeFrom child) ->
@@ -215,10 +220,7 @@ let rec prune : 'a 'b. ('a, 'b) node -> unit = function n ->
   end;
   sample n
 
-
-(* graft :: IORef (Node a b) -> IO () *)
 let rec graft : 'a 'b. ('a, 'b) node -> unit = function n ->
-  (* writeLog ("graft " ++ name n) *)
   (* Format.printf "graft %s@." n.name; *)
   begin match n.state with
   | Marginalized _ ->
@@ -235,48 +237,44 @@ let rec graft : 'a 'b. ('a, 'b) node -> unit = function n ->
       end
   end
 
-(* obs :: MType b -> IORef (Node a b) -> M () *)
 let obs (type a) (type b): pstate -> b mtype -> (a, b) node -> unit =
   fun prob x n ->
   graft n;
-  observe (prob, x, n)
+  observe prob x n
 
-
-(* getValue :: IORef (Node a b) -> IO (MType b) *)
-let rec get_value n =
-  (* n <- readIORef nref *)
+let rec get_value: 'a 'b. ('a, 'b) node -> 'b mtype =
+  fun n ->
   begin match n.state with
-  | Realized x -> x
-  | _ ->
-      graft n;
-      sample n;
-      get_value n
+    | Realized x -> x
+    | _ ->
+        graft n;
+        sample n;
+        get_value n
   end
 
-
 let print_state n =   (* XXX TODO XXX *)
-  Format.printf "%s:" n.name;
+  Format.printf "%s: " n.name;
   begin match n.state with
   | Initialized -> Format.printf "Initialized"
   | Marginalized (MGaussian (mu, var)) ->
       Format.printf "Marginalized (MGaussian (%f, %f))" mu var
+  | Marginalized _ -> Format.printf "Marginalized"
   | Realized x -> Format.printf "Realized %f" x
   end;
   Format.printf "@."
 
-(* observeConditional :: Eq (MType b) => Eq (MType c) => String -> IORef (Node a b) -> CDistr b c -> MType c -> M () *)
-let observe_conditional (type a) (type b) (type c): pstate -> string -> (a, b) node -> (b, c) cdistr -> c mtype -> unit =
+let observe_conditional (type a) (type b) (type c):
+  pstate -> string -> (a, b) node -> (b, c) cdistr -> c mtype -> unit =
   fun prob str n cdistr observation ->
   let y = assume_conditional str n cdistr in
   obs prob observation y
 
 
+open Ztypes
 
 type 'a infer_state =
   { znode_state : 'a;
     pstate : pstate; }
-
-open Ztypes
 
 let infer (Node { alloc; reset; step }) =
   let alloc () =
