fdd41cd0344bf5fa7a01447e0be044bc7e36ab39
Eric Atkinson <eatkinson@csail.mit.edu>
2019-04-03 22:03:16 +0000

    Coin

--- probabilistic/examples/benchmarks/coin/particles/aux.ml
+++ probabilistic/examples/benchmarks/coin/particles/aux.ml
@@ -1,5 +1,31 @@
 open Infer_ds_nogc;;
 
+let warmup = ref 0 ;;
+let perf = ref false;;
+let perf_step = ref false;;
+let mem = ref false;;
+let select_particle = ref None;;
+
+Arg.parse[
+    ("-w", Set_int warmup, "Numberof warmup iterations"); 
+    ("-perf", Unit (fun _ -> perf := true), "Performance testing");
+    ("-perf-step", Unit (fun _ -> perf_step := true), "Performance testing on a per step basis");
+    ("-mem", Unit (fun _ -> mem := true), "Memory performance testing");
+    ("-particles", Int (fun i -> select_particle := Some i), "Number of particles (single run)")
+] (fun _ -> ()) "Kalman particles test harness";;
+
+let parts = ref 
+    begin match !select_particle with
+    | Some i -> i
+    | None -> 10
+    end
+;;
+
+let particles _ =
+    !parts
+;;
+
+
 (*
 let read_val : unit -> (float * float) option =
     fun _ ->
@@ -9,33 +35,35 @@ let read_val : unit -> (float * float) option =
 ;;
 *)
 
-let read_val : unit ->  float * bool =
+let read_val : unit ->  float * float =
     fun _ ->
-        Scanf.scanf "%f, %B\n" (fun t o -> (t, o))
+        Scanf.scanf "%f, %f\n" (fun t o -> (t, o))
 ;;
 
 let get_mean (type a) : (a, float) random_var -> float =
     fun r ->
         match r.state with
-        | Marginalized (MGaussian (mu, sigma)) -> mu
+        | Marginalized (MBeta (a, b)) -> a /. (a +. b)
         | _ -> assert false (* error *)
 ;;
 
-let init_random : unit -> unit =
-    fun _ -> Random.self_init ()
+let random_init : unit -> unit =
+    fun _ ->
+        Random.self_init ()
 ;;
 
 let collect_garbage : unit -> unit =
-    fun _ -> Gc.full_major ()
+    fun _ -> (*Gc.full_major ()*)()
 ;;
 
 let get_memory : unit -> float =
     fun _ ->
-        let st = Gc.stat () in
-        float_of_int st.live_words
+        (*let st = Gc.stat () in
+        float_of_int st.live_words*)
+        0.0
 ;;
 
 let get_time : unit -> float =
     fun _ ->
-        Sys.time ()
+        (*Sys.time ()*)0.0
 ;;
