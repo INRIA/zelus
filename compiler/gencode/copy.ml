8d8e2e09e23ad9a12f463dbe6be1a0b50596e1a2
Marc Pouzet <you@example.com>
2019-05-08 12:39:52 +0200

    Elimination of copy variables.

---
+++ compiler/gencode/copy.ml
@@ -0,0 +1,69 @@
+(**************************************************************************)
+(*                                                                        *)
+(*                                Zelus                                   *)
+(*               A synchronous language for hybrid systems                *)
+(*                       http://zelus.di.ens.fr                           *)
+(*                                                                        *)
+(*                    Marc Pouzet and Timothy Bourke                      *)
+(*                                                                        *)
+(*  Copyright 2012 - 2019. All rights reserved.                           *)
+(*                                                                        *)
+(*  This file is distributed under the terms of the CeCILL-C licence      *)
+(*                                                                        *)
+(*  Zelus is developed in the INRIA PARKAS team.                          *)
+(*                                                                        *)
+(**************************************************************************)
+
+(* add a copy method which copies the state into a destination *)
+(* [method copy dest] recursively copies the internal state into [dest] *)
+
+open Misc
+open Ident
+open Lident
+open Deftypes
+open Obc
+open Format
+
+(* read/write of a state variable. *)
+let state is_read v k =
+  match k with
+  | None -> v
+  | Some(k) ->
+     match k with
+     | Deftypes.Cont ->
+	Oleft_state_primitive_access(v, Ocont)
+     | Deftypes.Zero ->
+	Oleft_state_primitive_access
+	  (v, if is_read then Ozero_in else Ozero_out)
+     | Deftypes.Horizon | Deftypes.Period | Deftypes.Encore -> v
+
+let copy_memory dest inst_list { m_name; m_typ; m_kind; m_size } =
+  Oassign_state(state false (Oleft_state_name(dest)) m_kind,
+		Ostate(state true Oself m_kind)) :: inst_list
+
+let copy_instance dest inst_list { i_name; i_machine; i_size } =
+  inst_list
+
+let machine f ({ ma_memories; ma_instances; ma_methods } as mach) =
+  let dest = Ident.fresh "dest" in
+  { mach with
+    ma_methods =
+      { me_name = Ohorizon;
+	me_params =
+	  [Ovarpat(dest, Otypevar("'a"))];
+	me_body =
+	  Osequence(List.fold_left
+		      (copy_memory dest)
+		      (List.fold_left (copy_instance dest) [] ma_instances)
+		      ma_memories);
+	me_typ = Initial.typ_unit } :: ma_methods }
+    
+  (** The main entry. *)
+let implementation impl =
+  match impl with
+  | Oletmachine(f, mach) -> 
+     (* only continuous machines are concerned *)
+     Oletmachine(f, machine f mach)
+  | Oletvalue _ | Oletfun _ | Oopen _ | Otypedecl _ -> impl
+									 
+let implementation_list impl_list = Misc.iter implementation impl_list
