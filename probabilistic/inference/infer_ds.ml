b408ed7e19e676fc8411aff764cc94e7b6ebf228
Eric Atkinson <eatkinson@csail.mit.edu>
2019-07-02 01:49:20 +0000

    Remove memory leak

--- probabilistic/inference/infer_ds.ml
+++ probabilistic/inference/infer_ds.ml
@@ -6,42 +6,72 @@ let factor = Infer_ds_ll.factor
 
 let infer = Infer_ds_ll.infer
 
-type 'a random_var =
- RV : ('b, 'a) Infer_ds_ll.rv -> 'a random_var
+type 'a random_var = RV : ('b, 'a) Infer_ds_ll.rv -> 'a random_var
 
-type _ expr =
-  | Econst : 'a -> 'a expr
-  | Ervar : 'a random_var -> 'a expr
-  | Eplus : float expr * float expr -> float expr
-  | Emult : float expr * float expr -> float expr
+type _ expr_tree =
+    | Econst : 'a -> 'a expr_tree
+    | Ervar : 'a random_var -> 'a expr_tree
+    | Eplus : float expr * float expr -> float expr_tree
+    | Emult : float expr * float expr -> float expr_tree
+and 'a expr = {
+    mutable value : 'a expr_tree;
+}
 
-let const v = Econst v
-let rvar x = Ervar x
-let plus e1 e2 = Eplus (e1, e2)
-let mult e1 e2 = Emult (e1, e2)
+let const : 'a. 'a -> 'a expr = 
+    begin fun v ->
+        {
+            value = Econst v;
+        }
+    end
+
+let plus : float expr -> float expr -> float expr =
+    begin fun e1 e2 ->
+        {
+            value = Eplus (e1, e2);
+        }
+    end
+
+let mult : float expr -> float expr -> float expr =
+    begin fun e1 e2 ->
+        {
+            value = Emult(e1,e2);
+        }
+    end
 
 type 'a distribution =
   | DS_gaussian of float expr * float
 
-
 let rec eval_float : float expr -> float =
-  function e ->
-    begin match e with
-    | Econst v -> v
-    | Ervar (RV x) -> Infer_ds_ll.get_value x
-    | Eplus (e1, e2) -> eval_float e1 +. eval_float e2
-    | Emult (e1, e2) -> eval_float e1 *. eval_float e2
+    begin fun e ->
+        let ret = 
+            begin match e.value with
+            | Econst v -> v
+            | Ervar (RV x) -> Infer_ds_ll.get_value x
+            | Eplus (e1, e2) -> eval_float e1 +. eval_float e2
+            | Emult (e1, e2) -> eval_float e1 *. eval_float e2
+            end
+        in
+
+        e.value <- Econst ret;
+        ret
     end
 
 let eval_bool : bool expr -> bool =
-    function e ->
-    begin match e with
-    | Econst v -> v
-    | Ervar (RV x) -> Infer_ds_ll.get_value x
+    begin fun e ->
+        let ret =
+            begin match e.value with
+            | Econst v -> v
+            | Ervar (RV x) -> Infer_ds_ll.get_value x
+            end
+        in
+
+        e.value <- Econst ret;
+        ret
     end
 
+
 let rec string_of_expr e = 
-    begin match e with
+    begin match e.value with
     | Econst v -> string_of_float v
     | Ervar (RV x) -> "Random"
     | Eplus (e1, e2) -> "(" ^ string_of_expr e1 ^ " + " ^ string_of_expr e2 ^ ")"
@@ -51,12 +81,12 @@ let rec string_of_expr e =
 
 let rec mean_expr : float expr -> float =
   function e ->
-    begin match e with
+    begin match e.value with
     | Econst v -> v
     | Ervar (RV x) -> Infer_ds_ll.mean x
     | Eplus (e1, e2) -> mean_expr e1 +. mean_expr e2
-    | Emult (e, Econst m)
-    | Emult (Econst m, e) ->
+    | Emult (e, {value = Econst m})
+    | Emult ({value = Econst m}, e) ->
         m *. mean_expr e
     | Emult (_, _) ->
       (print_string "Cannot compute the mean of multiplication.\n");
@@ -76,7 +106,7 @@ let observe (prob, ds_distr, o) =
   ds_distr.iobserve(prob, o)
 
 let ds_distr_of_distr d =
-  { isample = (fun prob -> Econst (Distribution.draw d));
+  { isample = (fun prob -> const (Distribution.draw d));
     iobserve = (fun (prob, obs) -> factor (prob, Distribution.score d obs)); }
 
 let ds_distr_with_fallback d is iobs =
@@ -106,98 +136,8 @@ let ds_distr_with_fallback d is iobs =
   { isample = is'; iobserve = iobs'; }
 
 
-let union_assoc =
-  let rec union_assoc_sorted f l1 l2 =
-    begin match l1, l2 with
-      | [], l | l, [] -> l
-      | (k1, v1) :: l1', (k2, v2) :: l2' ->
-          if k1 = k2 then
-            (k1, f k1 v1 v2) :: union_assoc_sorted f l1' l2'
-          else if k1 < k2 then
-            (k1, v1) :: union_assoc_sorted f l1' l2
-          else
-            (k2, v2) :: union_assoc_sorted f l1 l2'
-    end
-  in
-  fun f l1 l2 ->
-    union_assoc_sorted f (List.sort compare l1) (List.sort compare l2)
-
-(* getAffine :: Num a => Expr a -> Maybe (Map (Maybe (RefNodeToT a)) a) *)
-let rec get_affine e =
-    begin match e with
-    | Econst x -> Some [None, x]
-    | Ervar i -> Some [Some i, 1.]
-    | Eplus (e1, e2) ->
-        begin match get_affine e1, get_affine e2 with
-        | Some a1, Some a2 ->
-            Some (union_assoc (fun _ v1 v2 -> v1 +. v2) a1 a2)
-        | _, _ -> None
-        end
-    | Emult (e1, e2) ->
-        begin match get_affine e1, get_affine e2 with
-        | Some a1, Some a2 ->
-            let exception Stop in
-            let mult (x1, v1) (x2, v2) =
-              begin match x1, x2 with
-              | None, None -> None, v1 *. v2
-              | Some x, None | None, Some x -> Some x, v1 *. v2
-              | Some _, Some _ -> raise Stop
-              end
-            in
-            begin try
-              Some
-                (List.fold_left
-                   (fun acc (x1, v1) ->
-                      let l =
-                        List.map (fun (x2, v2) -> mult (x1, v2) (x2, v2)) a2
-                      in
-                      union_assoc (fun _ v1 v2 -> v1 +. v2) acc l)
-                   [] a1)
-            with Stop -> None
-            end
-            (* Some (union_assoc (fun _ x y -> x +. y) a1 a2) *)
-        | _, _ -> None
-        end
-    end
-
-(* toAffine :: Num v => Map (Maybe k) v -> Maybe (v, Maybe (k, v)) *)
-let to_affine p =
-  begin match p with
-  | None -> None
-  | Some [] -> Some (0., None)
-  | Some [ (None, v) ] -> Some (v, None)
-  | Some [ (Some x, v) ] -> Some (0., Some (x, v))
-  | Some [ (None, v); (Some x, vx) ]
-  | Some [ (Some x, vx); (None, v) ] -> Some (v, Some (x, v))
-  | _ -> None
-  end
-
-let get_affine1 e = to_affine (get_affine e)
-
-
-
-
-(* realizedToConst :: Expr a -> IO (Expr a) *)
-let const_of_realized =
-  let var_map rvar =
-    begin match rvar with
-    | RV { state = Realized x } -> Some x
-    | _ -> None
-    end
-  in
-  let rec f expr =
-    begin match expr with
-      | Econst x -> Econst x
-      | Ervar i ->
-          begin match var_map i with
-            | None -> Ervar i
-            | Some k -> Econst k
-          end
-      | Eplus (x, y) -> Eplus (f x, f y)
-      | Emult (x, y) -> Emult (f x, f y)
-    end
-  in
-  f
+let type_of_random_var (RV rv) =
+  Infer_ds_ll.type_of_dsdistr rv.distr
 
 (* An affine_expr is either a constant or an affine transformation of a
  * random variable *)
@@ -205,46 +145,28 @@ type affine_expr =
     (* Interpretation (m, x, b) such that the output is m * x + b *)
     | AErvar of float * float random_var * float
     | AEconst of float
-    | AEnone
 
-(* Performs constant propagation and extracts affine transformations from
- * expressions *)
-let rec ae_of_expr : float expr -> affine_expr =
-    begin fun e ->
-        begin match e with
-        | Econst v -> AEconst v
-        | Ervar var ->
-            begin match var with
-            | RV ({state = Realized x}) -> AEconst x
-            | _ -> AErvar (1., var, 0.)
-            end
+let rec affine_of_expr : float expr -> affine_expr option =
+    begin fun expr ->
+        begin match expr.value with
+        | Econst v -> Some (AEconst v)
+        | Ervar var -> Some (AErvar (1., var, 0.))
         | Eplus (e1, e2) ->
-            begin match (ae_of_expr e1, ae_of_expr e2) with
-            | (AErvar(m, x, b), AEconst v)
-            | (AEconst v, AErvar(m, x, b)) ->
-                AErvar(m, x, b +. v)
-            | (AEconst v1, AEconst v2) -> AEconst (v1 +. v2)
-            | _ -> AEnone
+            begin match (affine_of_expr e1, affine_of_expr e2) with
+            | (Some (AErvar (m, x, b)), Some (AEconst v))
+            | (Some (AEconst v), Some (AErvar (m, x, b))) -> Some (AErvar (m, x, b +. v))
+            | _ -> None
             end
         | Emult (e1, e2) ->
-            begin match (ae_of_expr e1, ae_of_expr e2) with
-            | (AErvar(m, x, b), AEconst v)
-            | (AEconst v, AErvar(m, x, b)) ->
-                AErvar(m *. v, x, b)
-            | (AEconst v1, AEconst v2) -> AEconst (v1 *. v2)
-            | _ -> AEnone
+            begin match (affine_of_expr e1, affine_of_expr e2) with
+            | (Some (AErvar (m, x, b)), Some (AEconst v))
+            | (Some (AEconst v), Some (AErvar (m, x, b))) -> Some (AErvar (m *. v, x, b *. v))
+            | _ -> None
             end
         end
     end
 
 
-
-(* forgettableVar :: IORef (Node a b) -> M (Expr (MType b)) *)
-let forgettable_var rv =
-  Ervar (RV rv)
-
-let type_of_random_var (RV rv) =
-  Infer_ds_ll.type_of_dsdistr rv.distr
 
 (** Gaussian distribution (gaussianPD in Haskell) *)
 let gaussian mu std =
@@ -271,19 +193,19 @@ let gaussian mu std =
     end
     *)
 
-    begin match ae_of_expr mu with
-    | AEconst v ->
+    begin match affine_of_expr mu with
+    | Some (AEconst v) ->
       let rv = Infer_ds_ll.assume_constant "" (MGaussian(v, std)) in
-      Some (forgettable_var rv)
-    | AErvar (m, RV x, b) ->
+      Some { value = (Ervar (RV rv)) }
+    | Some (AErvar (m, RV x, b)) ->
       let ty = type_of_random_var (RV x) in
       begin match ty with
       | MGaussianT ->
         let rv = Infer_ds_ll.assume_conditional "" x (AffineMeanGaussian(m, b, std)) in
-        Some (forgettable_var rv)
+        Some { value = (Ervar (RV rv)) }
       | _ -> None
       end
-    | _ -> None
+    | None -> None
     end
   in
   let iobs (prob, obs) =
@@ -304,17 +226,17 @@ let gaussian mu std =
           end
     end
     *)
-    begin match ae_of_expr mu with
-    | AEconst v ->
+    begin match affine_of_expr mu with
+    | Some (AEconst v) ->
       None
-    | AErvar (m, RV x, b) ->
+    | Some (AErvar (m, RV x, b)) ->
       let ty = type_of_random_var (RV x) in
       begin match ty with
       | MGaussianT ->
         Some (Infer_ds_ll.observe_conditional prob "" x (AffineMeanGaussian(m, b, std)) obs)
       | _ -> None
       end
-    | _ -> None
+    | None -> None
     end
   in
   ds_distr_with_fallback d is iobs
@@ -323,7 +245,7 @@ let gaussian mu std =
 (** Beta distribution (betaPD in Haskell) *)
 let beta a b =
   let d () = Distribution.beta a b in
-  let is prob = Some (forgettable_var (Infer_ds_ll.assume_constant "" (MBeta (a, b)))) in
+  let is prob = Some {value = Ervar (RV (Infer_ds_ll.assume_constant "" (MBeta (a, b)))) } in
   let iobs (pstate, obs) = None in
   ds_distr_with_fallback d is iobs
 
@@ -331,7 +253,7 @@ let beta a b =
 let bernoulli p =
   let d () = Distribution.bernoulli (eval_float p) in
   let with_beta_prior f =
-    begin match p with
+    begin match p.value with
       | Ervar (RV par) ->
           let ty = type_of_random_var (RV par) in
           begin match ty with
@@ -344,7 +266,7 @@ let bernoulli p =
   let is prob =
     with_beta_prior
       (fun (RV par) ->
-         forgettable_var (Infer_ds_ll.assume_conditional "" par Infer_ds_ll.CBernoulli))
+          { value = Ervar (RV (Infer_ds_ll.assume_conditional "" par Infer_ds_ll.CBernoulli)) })
   in
   let iobs (prob, obs) =
     with_beta_prior
@@ -352,77 +274,4 @@ let bernoulli p =
   in
   ds_distr_with_fallback d is iobs
 
-(** Computation on expressions *)
 
-type 'a result =
-  | RConst of 'a
-  | RMarginal of 'a Infer_ds_ll.mdistr
-
-let mean r =
-  begin match r with
-  | RConst x -> x
-  | RMarginal m -> Infer_ds_ll.mdistr_mean m
-  end
-
-let add_result r1 r2 =
-  begin match r1, r2 with
-  | RConst x, RConst y -> Some (RConst (x +. y))
-  | RConst x, RMarginal (MGaussian (mu, var))
-  | RMarginal (MGaussian (mu, var)), RConst x ->
-      Some (RMarginal (MGaussian (mu +. x, var)))
-  | _, _ -> None
-  end
-
-let negate_result r =
-  begin match r with
-  | RConst x -> Some (RConst (-. x))
-  | RMarginal (MGaussian (mu, var)) -> Some (RMarginal (MGaussian (-. mu, var)))
-  | _ -> None
-  end
-
-let mult_result r1 r2 =
-  begin match r1, r2 with
-  | RConst x, RConst y -> Some (RConst (x *. y))
-  | RConst x, RMarginal (MGaussian (mu, var))
-  | RMarginal (MGaussian (mu, var)), RConst x ->
-      Some (RMarginal (MGaussian (mu *. x, x *. x *. var)))
-  | _, _ -> None
-  end
-
-
-(* This is currently unsound *)
-(* Consider *)
-(* x <- normal(0, 1) *)
-(* pure (x + (- x)) *)
-let rec marginal_opt e =
-  begin match e with
-  | Econst x -> Some (RConst x)
-  | Ervar (RV n) ->
-      begin match n.state with
-      | Infer_ds_ll.Realized x -> Some (RConst x)
-      | Marginalized d ->
-          if Infer_ds_ll.stale n then None else Some (RMarginal d)
-      | Initialized -> Some (RMarginal (Infer_ds_ll.initialized_marginal n))
-      end
-  | Eplus (x, y) -> (* XXX unsound XXX *)
-      let mx = marginal_opt x in
-      let my = marginal_opt y in
-      begin match mx, my with
-      | Some mx, Some my -> add_result mx my
-      | _ -> None
-      end
-  | Emult (x, y) -> (* XXX unsound XXX *)
-      let mx = marginal_opt x in
-      let my = marginal_opt y in
-      begin match mx, my with
-      | Some mx, Some my -> mult_result mx my
-      | _ -> None
-      end
-  end
-
-
-let marginal e =
-  begin match marginal_opt e with
-  | Some x -> x
-  | None -> (RConst 0.) (* assert false *) (* XXX TODO XXX *)
-  end
