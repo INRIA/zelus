af543b836a2a5f7354fa55df48e8ac1d5852ec8e
Marc Pouzet <you@example.com>
2019-10-04 11:23:57 +0200

    Small comment.

--- lib/run.ml
+++ lib/run.ml
@@ -6,6 +6,8 @@ type 'a mem = { mutable m: 'a }
 
 type 'a instance = I: { s : 's; t : 's -> 'a } -> 'a instance
 
+(* [irun(f, x)] takes two streams [f : 'a -D-> 'b] and [x: 'a] *)
+(* and computes [f(0)(x)] *)
 let irun =
   let alloc () = { m = None } in
   let step self (f, x) =
