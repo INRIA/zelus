ec1cbc82885ec6f1a48b74c670dbfc2ba623a269
Louis Mandel <lmandel@us.ibm.com>
2019-10-07 10:56:43 -0400

    Add slam benchmark.

---
+++ probabilistic/examples/benchmarks/slam/ds_nogc/run.ml
@@ -0,0 +1,21 @@
+module M = struct
+  type input = (bool array * int) * (bool * int)
+  type output = unit
+
+  let read_input () =
+    let a = Array.make (Array_misc.max_pos + 1) false in
+    for i = 0 to Array_misc.max_pos do
+      a.(i) <- Scanf.scanf "%B, " (fun x -> x)
+    done;
+    let x = Scanf.scanf "%d, " (fun x -> x) in
+    let obs = Scanf.scanf "%B, " (fun x -> x) in
+    let cmd = Scanf.scanf "%d\n" (fun x -> x) in
+    (a, x), (obs, cmd)
+
+  let main = Slam_ds_nogc.main
+end
+
+module H = Harness.Make(M)
+
+let () =
+  H.run ()
