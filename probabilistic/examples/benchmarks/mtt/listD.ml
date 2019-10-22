4a0528717ca6ad08d2549fa1380b039944086227
Louis Mandel <lmandel@us.ibm.com>
2019-10-22 15:02:03 -0400

    listD

--- probabilistic/examples/benchmarks/mtt/listD.ml
+++ probabilistic/examples/benchmarks/mtt/listD.ml
@@ -1,2 +1,13 @@
-let map _ = assert false
-let ini _ = assert false
+open Ztypes
+
+let iter2_proba (Cnode { alloc; reset; copy; step; }) =
+  let step state (pstate, (l1, l2)) =
+    List.iter2 (fun x y -> step state (pstate, (x, y))) l1 l2
+  in
+  Cnode { alloc; reset; copy; step; }
+
+let map (Cnode { alloc; reset; copy; step; }) =
+  let step state l =
+    List.map (fun x -> step state x) l
+  in
+  Cnode { alloc; reset; copy; step; }
