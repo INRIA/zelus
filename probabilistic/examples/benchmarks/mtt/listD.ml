5decfa2b87b223ae6c0cdb1270c547d86567f9a5
Louis Mandel <lmandel@us.ibm.com>
2019-10-22 15:35:44 -0400

    Probabilistic operations on lists.

--- probabilistic/examples/benchmarks/mtt/listD.ml
+++ probabilistic/examples/benchmarks/mtt/listD.ml
@@ -1,11 +1,5 @@
 open Ztypes
 
-let iter2_proba (Cnode { alloc; reset; copy; step; }) =
-  let step state (pstate, (l1, l2)) =
-    List.iter2 (fun x y -> step state (pstate, (x, y))) l1 l2
-  in
-  Cnode { alloc; reset; copy; step; }
-
 let map (Cnode { alloc; reset; copy; step; }) =
   let step state l =
     List.map (fun x -> step state x) l
