90fde75116f87bb5d4fd599080a118cff0f433b0
Eric Atkinson <eatkinson@csail.mit.edu>
2019-10-26 22:23:40 +0000

    Complete data generation

--- probabilistic/examples/benchmarks/mtt/util.ml
+++ probabilistic/examples/benchmarks/mtt/util.ml
@@ -1,49 +1,68 @@
 open Owl
 
+let tdiff = 1.0
+
 let list_init = List.init
 
-let p_dead = 1. (* XXX TODO XXX *)
+let death_rate = 0.02
 
-let mu_new = (* XXX TODO XXX *)
+let p_dead = exp (-. tdiff *. death_rate)
+
+let mu_new = 
   Mat.of_arrays [| [| 0.; |];
-                   [| 1.; |]; |]
+                   [| 0.; |];
+                   [| 0.; |];
+                   [| 0.; |]; |]
 
-let sigma_new = (* XXX TODO XXX *)
-  Mat.of_arrays [| [| 1.0; 0.8; |];
-                   [| 0.8; 1.0; |] |]
+let sigma_new = 
+  Mat.of_arrays [| [| 1.0; 0.0; 0.0; 0.0; |];
+                   [| 0.0; 1.0; 0.0; 0.0; |];
+                   [| 0.0; 0.0; 0.001; 0.0; |];
+                   [| 0.0; 0.0; 0.0; 0.001; |]; |]
 
-let a_u = (* XXX TODO XXX *)
-  Mat.of_arrays [| [| 1.0; 0.8; |];
-                   [| 0.8; 1.0; |] |]
+let a_u = 
+  Mat.of_arrays [| [| 1.0; 0.0; tdiff; 0.0 |];
+                   [| 0.0; 1.0; 0.0; tdiff |];
+                   [| 0.0; 0.0; 1.0; 0.0   |];          
+                   [| 0.0; 0.0; 0.0; 1.0   |]; |]
 
-let b_u = (* XXX TODO XXX *)
+let b_u = 
   Mat.of_arrays [| [| 0.; |];
-                   [| 1.; |]; |]
+                   [| 0.; |]; 
+                   [| 0.; |]; 
+                   [| 0.; |]; |]
 
-let sigma_update = (* XXX TODO XXX *)
-  Mat.of_arrays [| [| 1.0; 0.8; |];
-                   [| 0.8; 1.0; |] |]
+let sigma_update = 
+  Mat.of_arrays [| [| 0.01; 0.0; 0.0; 0.0 |];
+                   [| 0.0; 0.01; 0.0; 0.0 |];
+                   [| 0.0; 0.0;  0.1; 0.0 |];
+                   [| 0.0; 0.0;  0.0; 0.1 |]; |]
 
-
-let mu_clutter = (* XXX TODO XXX *)
+let mu_clutter = 
   Mat.of_arrays [| [| 0.; |];
-                   [| 1.; |]; |]
+                   [| 0.; |]; |]
 
-let sigma_clutter = (* XXX TODO XXX *)
-  Mat.of_arrays [| [| 1.0; 0.8; |];
-                   [| 0.8; 1.0; |] |]
+let proj_pos =
+  Mat.of_arrays [| [| 1.0; 0.0; 0.0; 0.0 |];
+                   [| 0.0; 1.0; 0.0; 0.0 |]; |]
 
+let sigma_obs =
+  Mat.of_arrays [| [| 1.0; 0.0 |];
+                   [| 0.0; 1.0 |]; |]
 
-let mu_init = (* XXX TODO XXX *)
-  Mat.of_arrays [| [| 0.; |];
-                   [| 1.; |]; |]
-
-let sigma_init = (* XXX TODO XXX *)
-  Mat.of_arrays [| [| 1.0; 0.8; |];
-                   [| 0.8; 1.0; |] |]
-
+let sigma_clutter = 
+  Mat.of_arrays [| [| 10.0; 0.0 |];
+                   [| 0.0; 10.0 |]; |]
 
 let shuffle x = x
 
 let ( *@ ) = Mat.( *@ )
 let ( +@ ) = Mat.add
+
+let string_of_vec2_list vec_lst =
+  "[" ^
+  String.concat "," (List.map (fun vec ->
+    "(" ^ string_of_float (Mat.get vec 0 0) ^ ", " ^ 
+    string_of_float (Mat.get vec 1 0) ^ ")"
+  ) vec_lst)
+  ^ "]\n" 
