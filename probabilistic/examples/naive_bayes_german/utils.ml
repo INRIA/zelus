1c2519b5dcf1f9038277380b552e3105f02f872e
Guillaume Baudart <guillaume.baudart@ibm.com>
2019-11-04 16:24:30 -0500

    Example naive bayes

---
+++ probabilistic/examples/naive_bayes_german/utils.ml
@@ -0,0 +1,48 @@
+open Owl
+open Owl_plplot
+
+type mat = Mat.mat
+
+let shape = Mat.shape
+let row = Mat.row
+let col = Mat.col
+let get = Mat.get
+let add = Mat.add
+let add_scalar = Mat.add_scalar
+let elt_greater = Mat.elt_greater
+let elt_equal = Mat.elt_equal
+let map f (params, m) = Mat.map (fun x -> f (params, x)) m
+let sum axis = Mat.sum ~axis:axis
+let load_txt = Mat.load_txt ~sep: " "
+
+let pca n_components train =
+  let x = Mat.(train - (mean ~axis:0 train)) in
+  let u,s,v = Linalg.D.svd x in
+  let k = n_components - 1 in
+  let v = Mat.(v.${[[]; [0; k]]}) in
+  fun test -> Mat.(test *@ v)
+
+let split_data p m =
+  let extract_labels m = 
+    Mat.(m.${[[]; [0;-2]]}),
+    Mat.(m.${[[]; [-1]]} -$ 1.)
+  in
+  let n, _ = Mat.shape m in
+  let tflag = Mat.bernoulli ~p:0.7 n 1 in
+  let train_idx = Mat.filter ((=) 1.) tflag in
+  let test_tidx = Mat.filter ((=) 0.) tflag in
+  extract_labels (Mat.rows m train_idx),
+  extract_labels (Mat.rows m test_tidx)
+
+let res = ref []
+let add_result acc = res := acc::!res
+
+let h = Plot.create "plot_accuracy.pdf";;
+Plot.set_page_size h 500 400
+
+let exit_and_plot () = 
+  let res = Array.of_list (List.rev !res) in
+  let res = Arr.of_array res [|Array.length res|] in
+  Plot.semilogx ~h ~spec:[ RGB (150,0,0) ] res;
+  Plot.output h;
+  exit 0
