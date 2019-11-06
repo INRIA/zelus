74c4430d2b1dce2a1c238b1feaba6f2b018eeb7c
Guillaume Baudart <guillaume.baudart@ibm.com>
2019-11-06 10:15:28 -0500

    more cleaning

--- probabilistic/examples/tracker/lib.ml
+++ probabilistic/examples/tracker/lib.ml
@@ -1,21 +1,10 @@
 open Owl
+open Owl_plplot
 
-let concat_vertical = Mat.concat_vertical
-let concat_horizontal = Mat.concat_horizontal
 let of_lists l = Mat.of_arrays (Array.of_list (List.map Array.of_list l))
-let eye = Mat.eye
-let zeros = Mat.zeros
-let mul_scalar = Mat.mul_scalar
-let dot = Mat.dot
-let add = Mat.add
-let diagm = Mat.diagm
-let get = Mat.get
-
 let dare = Linalg.D.dare
 let transpose = Mat.transpose
 let linsolve = Linalg.D.linsolve
-
-open Owl_plplot
 
 let plot file y =
   let h = Plot.create file in
