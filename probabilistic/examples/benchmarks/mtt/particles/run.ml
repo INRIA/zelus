d948da05223f678b13bfab8a87eb64646ee53466
Eric Atkinson <eatkinson@csail.mit.edu>
2019-11-07 03:23:53 +0000

    MTT Particle Filter Compiling

--- probabilistic/examples/benchmarks/mtt/particles/run.ml
+++ probabilistic/examples/benchmarks/mtt/particles/run.ml
@@ -1,29 +1,34 @@
 open Benchlib
+open Owl
 
 module M = struct
-  type input = ((float * float) list) * ((float * float) list)
+  type input = (Mat.mat list) * (Mat.mat list)
   type output = unit
 
   let read_input () =
+    let mk_vec a b =
+        Mat.of_arrays [| [| a |];
+                         [| b |] |]
+    in
     let process_list _ = 
-      let process_list_helper fst =
+      let rec process_list_helper fst =
         try
-          Scanf.scanf "]";
+          Scanf.scanf "]" (fun _ -> ()) 0;
           []
-        with Scan_failure _ ->
+        with Scanf.Scan_failure _ ->
           let this_elem = if fst then
-            Scanf.scanf "(%f, %f)" (fun x y -> (x, y)) 
+            Scanf.scanf "(%f, %f)" (fun x y -> mk_vec x y) 
           else 
-            Scanf.scanf ",(%f, %f)" (fun x y -> (x, y)) 
+            Scanf.scanf ",(%f, %f)" (fun x y -> mk_vec x y) 
           in
-          this_elem :: (process_list false)
+          this_elem :: (process_list_helper false)
       in
-      Scanf.scanf "[";
+      Scanf.scanf "[" (fun _ -> ()) 0;
       process_list_helper true
     in
 
     let truth = process_list () in
-    Scanf.scanf " : ";
+    Scanf.scanf " : " (fun _ -> ()) 0;
     let obs = process_list () in
     (truth, obs)
 
