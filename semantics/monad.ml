cb76764c578f1c75700bd0502393d469f9d8db42
Marc Pouzet <you@example.com>
2020-05-03 17:28:23 +0200

    Renamed let+ into let* (let+ seems to be reserved for lifting).

--- semantics/monad.ml
+++ semantics/monad.ml
@@ -1,35 +1,40 @@
 module Opt =
   struct
-    (* let+ x = e in f x *)
-    let (let+) e f =
+    (* let* x = e in f x *)
+    let (let*) e f =
       match e with
       | None -> None
       | Some(v) -> f v
                  
+    let (and*) v1 v2 =
+      match v1, v2 with
+      | None, _ | _, None -> None
+      | Some(v1), Some(v2) -> Some(v1, v2)
+                              
     let return v = Some(v)
 
     let rec map f x_list =
       match x_list with
       | [] -> return []
       | x :: x_list ->
-         let+ xv = f x in
-         let+ x_list = map f x_list in
+         let* xv = f x in
+         let* x_list = map f x_list in
          return (xv :: x_list)
 
     let rec fold f acc x_list =
       match x_list with
       | [] -> return acc
       | x :: x_list ->
-         let+ acc = f acc x in
-         let+ acc = fold f acc x_list in
+         let* acc = f acc x in
+         let* acc = fold f acc x_list in
          return acc
 
     let rec fold2 f acc x_list y_list =
       match x_list, y_list with
       | [], [] -> return acc
       | x :: x_list, y :: y_list ->
-         let+ acc = f acc x y in
-         let+ acc = fold2 f acc x_list y_list in
+         let* acc = f acc x y in
+         let* acc = fold2 f acc x_list y_list in
          return acc
       | _ -> None
 
@@ -37,8 +42,8 @@ module Opt =
       match x_list, y_list with
       | [], [] -> return []
       | x :: x_list, y :: y_list ->
-         let+ z = f x y in
-         let+ z_list = map2 f x_list y_list in
+         let* z = f x y in
+         let* z_list = map2 f x_list y_list in
          return (z :: z_list)
       | _ -> None
 
@@ -46,16 +51,16 @@ module Opt =
       match x_list with
       | [] -> return (acc, [])
       | x :: x_list ->
-         let+ acc, s = f acc x in
-         let+ acc, s_list = mapfold f acc x_list in
+         let* acc, s = f acc x in
+         let* acc, s_list = mapfold f acc x_list in
          return (acc, s :: s_list)
 
     let rec mapfold2 f acc x_list y_list =
       match x_list, y_list with
       | [], [] -> return (acc, [])
       | x :: x_list, y :: y_list ->
-         let+ acc, s = f acc x y in
-         let+ acc, s_list = mapfold2 f acc x_list y_list in
+         let* acc, s = f acc x y in
+         let* acc, s_list = mapfold2 f acc x_list y_list in
          return (acc, s :: s_list)
       | _ -> None
   end
@@ -69,7 +74,7 @@ module State =
       fun s -> match f s with (a, s') -> g a s'
 
     let return = ret
-    let (let+) = bind
+    let (let*) = bind
   end
 
 module Misc =
