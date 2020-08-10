393306423a2127fd216efe083792171ddf8f39d0
Guillaume Baudart <guillaume.baudart@ibm.com>
2020-08-10 23:13:46 +0200

    Simplify runtime and load librairies by name

--- pyzls/pyzls.py
+++ pyzls/pyzls.py
@@ -4,10 +4,12 @@ import ast
 from tempfile import NamedTemporaryFile
 from os.path import splitext, basename, dirname
 import importlib.util
-from typing import Dict
+from typing import Dict, Any
 from inspect import getsource
 import sys
 from functools import wraps
+import builtins
+import runpy
 
 
 class Node(ABC):
@@ -30,10 +32,7 @@ class CNode(Node):
         pass
 
 
-_pyzlslib: Dict[str, str] = {}
-
-
-def ml_type(f):
+def _ml_type(f):
     ty = " * ".join(
         [
             t.__name__ if hasattr(t, "__name__") else str(t)
@@ -46,61 +45,62 @@ def ml_type(f):
     return ty
 
 
-def zllib(f):
-    _pyzlslib[f.__name__] = {"type": ml_type(f), "py": getsource(f).split("\n", 1)[1]}
+def _compile_libs(libs):
+    for libname, lib in libs.items():
+        with open(libname + ".zli", "w") as fzli, open(libname + ".py", "w") as fpyl:
+            for f, d in lib.items():
+                fzli.write(f'val {f}: {d["type"]}\n')
+                fpyl.write(d["py"])
+            fzli.seek(0)
+            fpyl.seek(0)
+            subprocess.check_call(["../bin/zeluc", fzli.name])
+            if libname in sys.modules:
+                importlib.reload(sys.modules[libname])
 
-    @wraps(f)
-    def wrapper(*args):
-        return f(*args)
 
-    return wrapper
-
-
-class PyZL:
-    def __init__(self, src, opt=[]):
-        self.zl_code = src
-        with NamedTemporaryFile(
-            suffix=".zls", delete=False
-        ) as fzls, NamedTemporaryFile(suffix=".zli", delete=False) as fzli, open(
-            splitext(fzli.name)[0] + ".py", "w"
-        ) as fpyl:
-
-            # Compile lib
-            if _pyzlslib:
-                for f, d in _pyzlslib.items():
-                    fzli.write(f'val {f}: {d["type"]}\n'.encode())
-                    fpyl.write(d["py"])
-                fzli.seek(0)
-                fpyl.seek(0)
-                subprocess.check_call(["../bin/zeluc", fzli.name])
-                lib = splitext(basename(fzli.name))[0].capitalize()
-                fzls.write(f"open {lib}".encode())
-                sys.path.insert(0, dirname(fzli.name))
-
-            # Compile code
-            fzls.write(src.encode())
+def _compile_code(name, src, opt=[], libname=None):
+    with open(name + ".zls", "w") as fzls:
+        if libname:
+            fzls.write(f"open {libname.capitalize()}")
+            fzls.write(src)
             fzls.seek(0)
-            subprocess.check_call(
-                [
-                    "../bin/zeluc",
-                    "-python",
-                    "-noreduce",
-                    "-copy",
-                    f"-I {dirname(fzli.name)}",
-                ]
-                + opt
-                + [fzls.name]
-            )
-            with open(splitext(fzls.name)[0] + ".py") as fpy:
-                self.py_code = fpy.read()
+        subprocess.check_call(
+            ["../bin/zeluc", "-python", "-noreduce", "-copy",] + opt + [fzls.name]
+        )
 
-            # Load python module as class attributes
-            modname = splitext(fzls.name)[0]
-            spec = importlib.util.spec_from_file_location("zlpyc", modname + ".py")
-            mod = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(mod)
-            for name, cls in mod.__dict__.items():
-                setattr(self, name, cls)
 
-    def __repr__(self):
-        return f"(* Zelus code *)\n{self.zl_code}\n{'-'*80}\n\n# Python Code\n\n{self.py_code}"
+_libs: Dict[str, Any] = {"libtop": {}}
+
+
+def lib(libname):
+    def inner(f):
+        if libname not in _libs:
+            _libs[libname] = {}
+        _libs[libname][f.__name__] = {
+            "type": _ml_type(f),
+            "py": getsource(f).split("\n", 1)[1],
+        }
+
+        @wraps(f)
+        def wrapper(*args):
+            return f(*args)
+
+        return wrapper
+
+    return inner
+
+
+toplib = lib("libtop")
+
+
+def load(src, scope=builtins, opt=[]):
+    _compile_libs(_libs)
+    _compile_code("top", src, opt, libname="libtop")
+
+    # Load python module as class attribute
+    spec = importlib.util.spec_from_file_location("zlpyc", "top.py")
+    mod = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(mod)
+    for name, val in mod.__dict__.items():
+        if not name.startswith("__"):
+            setattr(scope, name, val)
