36243b29831c90de43f1612c861723ae5f6cad1f
rtekin <rtekin>
2021-03-19 11:17:00 +0100

    Fix "Pattern matching failed" when compiling with -muf or -mufpy. -> Surround the sub-blocks  of parttern matching with parentheses

--- compiler/muf/muf_gencode.ml
+++ compiler/muf/muf_gencode.ml
@@ -58,10 +58,12 @@ let rec compile_expr:
       (
       match e1.expr with
       | Evar v when v.name.[0] == '(' -> (* Infix operator *)
+          (
           match e2.expr with 
           | Etuple [op1;op2] as l -> (* Arguments of the operator as a tuple. Support only for binary operators (arguments as a tuple of size 2) *)
               Exp.apply (Exp.apply (compile_expr e1) [Nolabel, compile_expr op1]) [Nolabel, compile_expr op2]
           | _ -> Format.eprintf "Tuple of size 2 expected for the infix binary operator." ; assert false
+          )
       | _ -> Exp.apply (compile_expr e1) [Nolabel, compile_expr e2]
       )
     | Eif (e, e1, e2) ->
