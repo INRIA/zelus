2bde9432efe9e2f16bb719487e0838b3a7181776
rtekin <rtekin>
2021-04-29 16:51:39 +0200

    Define a compilation pass to flat the let expressions

---
+++ compiler/muf/muf_flatten.ml
@@ -0,0 +1,155 @@
+open Muf
+open Muf_rename
+
+let is_flat e =
+  let rec flat_expr acc e =
+    let acc =
+      begin match e.expr with
+      | Elet _ -> false
+      | _ -> acc
+      end
+    in
+    fold_expr_desc (fun acc _ -> acc) flat_expr acc e.expr
+  in 
+  flat_expr true e
+
+let rec flatten :
+  type a. (a pattern * a expression) list -> a expression -> (a pattern * a expression) list * a expression = begin
+    fun acc e ->
+      let acc, expr =
+        begin match e.expr with
+        | Econst _ -> acc, e.expr
+        | Evar _ -> acc, e.expr
+        | Efield (e, x) -> 
+          let acc, e = flatten acc e in
+          acc, Efield(e, x)
+        | Eapp (e1, e2) -> 
+          let acc, e1 = flatten acc e1 in
+          let acc, e2 = flatten acc e2 in
+          acc, Eapp(e1, e2)
+        | Eif (ec, et, ef) -> 
+          let acc, et = flatten acc et in
+          let acc, ef = flatten acc ef in
+          let acc, ec = flatten acc ec in
+          acc, Eif(ec, et, ef)
+        | Esequence (e1, e2) -> 
+          let acc, e1 = flatten acc e1 in
+          let acc, e2 = flatten acc e2 in
+          acc, Esequence(e1, e2)
+        | Ecall_init e -> 
+          let acc, e = flatten acc e in
+          acc, Ecall_init e
+        | Ecall_step (e1, e2) -> 
+          let acc, e1 = flatten acc e1 in
+          let acc, e2 = flatten acc e2 in
+          acc, Ecall_step(e1, e2)
+        | Ecall_reset e -> 
+          let acc, e = flatten acc e in
+          acc, Ecall_reset e
+        | Esample (prob, e) -> 
+          let acc, e = flatten acc e in
+          acc, Esample(prob, e)
+        | Eobserve (prob, e1, e2) -> 
+          let acc, e1 = flatten acc e1 in
+          let acc, e2 = flatten acc e2 in
+          acc, Eobserve(prob, e1, e2)
+        | Efactor (prob, e) -> 
+          let acc, e = flatten acc e in
+          acc, Efactor(prob, e)
+        | Einfer (e, id) -> 
+          let acc, e = flatten acc e in
+          acc, Einfer(e, id)
+        | Etuple le -> 
+          let rec f acc le acc_e = 
+            match le with
+            | [] -> acc, List.rev acc_e
+            | h::t -> 
+              let acc, e = flatten acc h in
+              f acc t (e::acc_e)
+          in
+          let acc, le = f acc le [] in
+          acc, Etuple le
+        | Erecord (l_se, oe) -> 
+          let rec f acc le acc_e = 
+            match le with
+            | [] -> acc, List.rev acc_e
+            | (s,h)::t -> 
+              let acc, e = flatten acc h in
+              f acc t ((s,e)::acc_e)
+          in
+          let acc, l_se = f acc l_se [] in
+          let acc, oe = 
+            begin match oe with 
+            | Some e -> 
+              let acc, e = flatten acc e in
+              acc, Some e
+            | _ -> acc, oe
+            end
+          in
+          acc, Erecord (l_se, oe)
+        | Elet(p, e1, e2) ->
+          let acc, e1 = flatten acc e1 in
+          let acc = (p, e1)::acc in
+          let acc, e2 = flatten acc e2 in 
+          acc, e2.expr
+        | Ematch _ -> assert false
+        | Econstr _ -> assert false
+        end
+      in acc, {e with expr=expr }
+    end
+
+let compile_flatten :
+  type a. a expression -> a expression = begin
+  fun e -> 
+    begin match is_flat e with
+      | true -> e
+      | false ->     
+      let decl, e = flatten [] e in
+      let f (p, e1) e2 = { e with expr = (Elet(p, e1, e2)) } in
+      List.fold_right f (List.rev decl) e
+      end
+  end
+
+let compile_node :
+  type a. (a pattern, a expression) decl_desc -> (a pattern, a expression) decl_desc = begin
+    fun d ->
+      begin match d with
+      | Dnode (f, params, n) -> 
+        let n' =
+          let compile_method_with_params (p, e) = (p, compile_flatten e) in
+          let compile_class n =
+            let ei = compile_flatten n.n_init in
+            let ps, es = compile_method_with_params n.n_step in 
+            {n with n_init = ei ; n_step = (ps, es)}
+          in compile_class n
+        in Dnode(f, params, n')
+      | _ -> d 
+    end
+  end
+
+let compile_decl :
+  type a. a declaration ->  a declaration = begin
+    fun d -> 
+      let decl =  
+      begin match d.decl with
+      | Ddecl (p, e) ->  
+        let e = compile_flatten e in 
+        Ddecl(p, e)
+      | Dfun (f, p, e) -> 
+        let e = compile_flatten e in 
+        Dfun (f, p, e)
+      | Dnode (f, p, n) as dn -> compile_node dn
+      | Dtype _ | Dopen _ -> d.decl
+      end
+    in {decl}
+  end
+
+let compile_program :
+  type a. a program -> a program = begin
+    fun p -> 
+      (* rename *)
+      let p = Muf_rename.compile_program p in
+      (* flatten *)
+      let p = List.map compile_decl p in
+      p
+  end
