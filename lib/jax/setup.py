133ab107017956afe69380bd1bbe807e05802da8
rtekin <rtekin>
2021-07-01 15:54:16 +0200

    Add some informations

--- lib/jax/setup.py
+++ lib/jax/setup.py
@@ -4,5 +4,8 @@ setup(
     name="zlax",
     version="2.2",
     packages=["zlax"],
+    description="Librairies for the Python backend of Zelus using JAX.",
+    author="Reyyan Tekin",
+    url="https://github.com/INRIA/zelus",
     install_requires=["jax"]
 )
