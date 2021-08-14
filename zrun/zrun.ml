802c423aeff82803405732686ea5e0b247ac3de9
Marc Pouzet <you@example.com>
2021-08-14 18:55:31 +0200

    Cleaning.

--- zrun/zrun.ml
+++ zrun/zrun.ml
@@ -12,7 +12,7 @@
 (* *********************************************************************)
 
 (* the main *)
-open Misc
+open Smisc
 open Monad
 open Opt
 open Value
@@ -46,46 +46,58 @@ let parse parsing_fun lexing_fun source_name =
      
 let parse_implementation_file source_name =
   parse Parser.implementation_file Lexer.main source_name
-
-(* The main entry function *)
-let run genv main ff n =
+               
+(* The main entry function. Execute [n] steps of [main()] or [run main ()]  *)
+let eval_main genv main n_steps =
   match main with
   | None -> ()
   | Some(main) ->
      let fv =
        find_gvalue_opt (Name main) genv in
      match fv with
-     | None -> Format.eprintf "@[The global value %s is unbound.@\n@]" main
+     | None -> Format.eprintf "The global value %s is unbound.\n" main
      | Some(fv) ->
         let v = to_fun fv in
         match v with
         | None ->
-           Format.eprintf "@[The global value %s is not a function.@\n@]" main
+           Format.eprintf "The global value %s is not a function.\n" main
         | Some(v) ->
+           let output v =
+             Output.pvalue_flush Format.std_formatter v in
            match v with
            | Vfun(fv) ->
-              run_fun (Output.pvalue_and_flush ff) fv n
+              run_fun output fv n_steps
            | Vnode { init; step } ->
-              run_node (Output.pvalue_and_flush ff) init step n
+              run_node output init step n_steps
            | _ -> assert false
- 
-(* evaluate the main node [main] given by option [-s] for [n_steps] steps *)
-let eval filename main n_steps =
-  (* standard output *)
-  let info_ff = Format.formatter_of_out_channel stdout in
-  Format.pp_set_max_boxes info_ff max_int;
 
-  (* standard error *)
-  let err_ff = Format.formatter_of_out_channel stderr in
-  Format.pp_set_max_boxes err_ff max_int;
+let apply_with_close_out f o =
+  try
+    f o;
+    close_out o
+  with x -> close_out o; raise x
 
-  let do_step comment step input = 
-    let output = step input in
-    Debug.print_message comment;
-    Debug.print_program output;
-    output in
+let do_step comment step input = 
+  let output = step input in
+  Debug.print_message comment;
+  Debug.print_program output;
+  output
 
-  let impl_list = parse_implementation_file filename in
+(* Evaluate all the definition in a file, store values *)
+(* and a main function/node, if given *)
+let eval_file modname filename suffix main n_steps =
+  (* output file in which values are stored *)
+  (*
+    let obj_name = filename ^ ".zlo" in
+    let otc = open_out_bin obj_name in
+   *)
+  
+  let source_name = filename ^ suffix in
+
+  Location.initialize source_name;
+
+  (* Parsing *)
+  let impl_list = parse_implementation_file source_name in
   Debug.print_message "Parsing done";
 
   (* Scoping *)
@@ -95,64 +107,61 @@ let eval filename main n_steps =
   let impl_list = do_step "Write done" Write.program impl_list in
 
   (* Evaluation of definitions in [filename] *)
-  let genv = Opt.get (Coiteration.program Primitives.genv0 impl_list) in
+  let genv = Genv.initialize modname [] in
+  (* Add Stdlib *)
+  let genv = Genv.add_module genv Primitives.stdlib_env in
+  
+  let genv = Opt.get (Coiteration.program genv impl_list) in
   Debug.print_message "Evaluation of definitions done";
 
-  (* Evaluate [f ()] or [run f ()] *)
-  run genv main Format.std_formatter n_steps
+  (* Write the values into a file *)
+  (* 
+    TODO
+    apply_with_close_out (Genv.write genv) otc;
+   *)
 
-let main filename =
-  if Filename.check_suffix filename ".zls"
+  (* evaluate a main function/node if given *)
+  eval_main genv main n_steps
+             
+let main file =
+  if Filename.check_suffix file ".zls"
   then 
-    begin
-      Location.initialize filename;
-      let _ = eval filename !Smisc.main_node !Smisc.number_of_steps in
-     if !Smisc.print_number_of_fixpoint_iterations
-      then Format.eprintf
-             "@[The number of fixpoint iterations is : %d@]@\n"
-             !Smisc.number_of_fixpoint_iterations
-    end
+    let filename = Filename.chop_extension file in
+    let modname = String.capitalize_ascii (Filename.basename filename) in
+    eval_file modname filename ".zls" !Smisc.main_node !Smisc.number_of_steps
   else raise (Arg.Bad "Expected *.zls file.")
-    
-                                                   
+                                                       
 let doc_main = "\tThe main node to evaluate"
 let doc_number_of_steps = "\tThe number of steps"
 let doc_verbose = "\tVerbose mode"
 let doc_vverbose = "\t Set even more verbose mode"
 let doc_no_assert = "\tNo check of assertions"
 let doc_nocausality = "\tTurn off the check that are variables are non bottom"
+let doc_print_values = "\tPrint values"
 let doc_number_of_fixpoint_iterations =
   "\tPrint the number of steps for fixpoints"
     
 let errmsg = "Options are:"
 
-let set_verbose () =
-  let open Misc in
-  verbose := true;
-  Printexc.record_backtrace true
-
-let set_vverbose () =
-  let open Misc in
-  vverbose := true;
-  set_verbose ()
 
 let main () =
   try
-    Arg.parse (Arg.align
-                 [ "-s", Arg.String Smisc.set_main, doc_main;
-                   "-n", Arg.Int Smisc.set_number_of_steps, doc_number_of_steps;
-                   "-v", Arg.Unit set_verbose, doc_verbose;
-                   "-vv", Arg.Unit set_vverbose, doc_vverbose;
-                   "-noassert", Arg.Set Smisc.no_assert, doc_no_assert;
-                   "-nocausality", Arg.Set Smisc.set_nocausality,
-                   doc_nocausality;
-                   "-fix", Arg.Set Smisc.print_number_of_fixpoint_iterations,
-                   doc_number_of_fixpoint_iterations;
-                   ])
+    Arg.parse
+      (Arg.align
+         [ "-s", Arg.String Smisc.set_main, doc_main;
+           "-n", Arg.Int Smisc.set_number_of_steps, doc_number_of_steps;
+           "-v", Arg.Unit set_verbose, doc_verbose;
+           "-vv", Arg.Unit set_vverbose, doc_vverbose;
+           "-iv", Arg.Set print_values, doc_print_values;
+           "-noassert", Arg.Set Smisc.no_assert, doc_no_assert;
+           "-nocausality", Arg.Set Smisc.set_nocausality, doc_nocausality;
+           "-fix", Arg.Set Smisc.print_number_of_fixpoint_iterations,
+           doc_number_of_fixpoint_iterations;
+      ])
       main
       errmsg
   with
-  | Error | Stdlib.Exit -> exit 2
+  | Stdlib.Exit -> exit 2
   
 let _ = main ()
 let _ = exit 0
