17d505d8720a072c2d8bc0119fc749b57f18be3d
Marc Pouzet <you@example.com>
2021-08-27 14:21:23 +0200

    Cleaning.

--- zrun/match.ml
+++ zrun/match.ml
@@ -19,22 +19,22 @@ open Opt
 open Ident
    
 (* match a value [v] against a pattern [p] *)
-let pmatching (v : pvalue) (p : pattern) =
-  let rec matchrec acc v { pat_desc } =
+let pmatch (v : pvalue) (p : pattern) : pvalue Env.t Opt.t =
+  let rec pmatch acc v { pat_desc } =
     match v, pat_desc with
-    | _, Etypeconstraintpat(p, _) -> matchrec acc v p
+    | _, Etypeconstraintpat(p, _) -> pmatch acc v p
     | _, Evarpat(x) ->
        return (Env.add x v acc)
     | _, Ewildpat -> return acc
     | _, Ealiaspat(p, x) ->
-       let* acc = matchrec acc v p in
+       let* acc = pmatch acc v p in
        return (Env.add x v acc)
     | _, Eorpat(p1, p2) ->
-       let env = matchrec Env.empty v p1 in
+       let env = pmatch acc v p1 in
        let acc =
          match env with
-         | None -> matchrec acc v p2
-         | Some(env) -> return (Env.append env acc) in
+         | None -> pmatch acc v p2
+         | Some(env) -> return env in
        acc
     | Vrecord(l_v_list), Erecordpat(l_p_list) ->
        let rec find l = function
@@ -44,35 +44,34 @@ let pmatching (v : pvalue) (p : pattern) =
        Opt.fold
          (fun acc { label; arg = p } ->
            let* v = find label l_v_list in
-           matchrec acc v p) acc l_p_list
-    | Vint(v1), Econstpat(Eint(v2)) when v1 = v2 -> return Env.empty
-    | Vbool(v1), Econstpat(Ebool(v2)) when v1 = v2 -> return Env.empty
-    | Vfloat(v1), Econstpat(Efloat(v2)) when v1 = v2 -> return Env.empty
-    | Vchar(v1), Econstpat(Echar(v2)) when v1 = v2 -> return Env.empty
-    | Vstring(v1), Econstpat(Estring(v2)) when v1 = v2 -> return Env.empty
-    | Vvoid, Econstpat(Evoid) -> return Env.empty
+           pmatch acc v p) acc l_p_list
+    | Vint(v1), Econstpat(Eint(v2)) when v1 = v2 -> return acc
+    | Vbool(v1), Econstpat(Ebool(v2)) when v1 = v2 -> return acc
+    | Vfloat(v1), Econstpat(Efloat(v2)) when v1 = v2 -> return acc
+    | Vchar(v1), Econstpat(Echar(v2)) when v1 = v2 -> return acc
+    | Vstring(v1), Econstpat(Estring(v2)) when v1 = v2 -> return acc
+    | Vvoid, Econstpat(Evoid) -> return acc
     | Vconstr0(f), Econstr0pat(g) when Lident.compare f g = 0 ->
-       return Env.empty
+       return acc
     | Vstuple(v_list), Etuplepat(p_list) ->
-       matchrec_list acc v_list p_list
+       pmatch_list acc v_list p_list
     | Vconstr1(f, v_list), Econstr1pat(g, p_list) when Lident.compare f g = 0 ->
-       matchrec_list acc v_list p_list
+       pmatch_list acc v_list p_list
     | _ -> none
          
-  and matchrec_list acc v_list p_list =
-    Opt.fold2 matchrec acc v_list p_list in
-  (* main entry *)
-  matchrec Env.empty v p
+  and pmatch_list acc v_list p_list = Opt.fold2 pmatch acc v_list p_list in
+
+  pmatch Env.empty v p
   
 (* pattern matching for equations [p = e] and function application *)
 (* [v] is an extended value; [p] is a pattern but pattern matching *)
 (* should not fail. In the case of a failure, this is considered as *)
 (* a typing error *)
-let matcheq (v : pvalue) (p : pattern) : pvalue Env.t Opt.t =
-  let rec matchrec acc v { pat_desc } =
+let pmatcheq (v : pvalue) (p : pattern) : pvalue Env.t Opt.t =
+  let rec pmatcheq acc v { pat_desc } =
     match v, pat_desc with
     | Vstuple(v_list), Etuplepat(l_list) ->
-       match_list acc v_list l_list
+       pmatcheq_list acc v_list l_list
     | Vrecord(l_v_list), Erecordpat(l_p_list) ->
        let rec find l = function
          | [] -> none
@@ -81,29 +80,29 @@ let matcheq (v : pvalue) (p : pattern) : pvalue Env.t Opt.t =
        Opt.fold
          (fun acc { label; arg = p } ->
            let* v = find label l_v_list in
-           matchrec acc v p) acc l_p_list
+           pmatcheq acc v p) acc l_p_list
     | _, Evarpat(x) ->
        return (Env.add x v acc)
     | _, Ewildpat -> return acc
     | _, Ealiaspat(p, x) ->
-       let* acc = matchrec acc v p in
+       let* acc = pmatcheq acc v p in
        return (Env.add x v acc)
     | _ -> none
-  and match_list acc v_list p_list =
+  and pmatcheq_list acc v_list p_list =
     match v_list, p_list with
     | [], [] -> return acc
     | v :: v_list, p :: p_list  ->
-       let* acc = matchrec acc v p in
-       match_list acc v_list p_list 
+       let* acc = pmatcheq acc v p in
+       pmatcheq_list acc v_list p_list 
     | _ -> none in
-  matchrec Env.empty v p
+  pmatcheq Env.empty v p
 
 (* Pattern matching of a signal *)
 let matchsig (vstate: pvalue) (p: pattern) : (pvalue * pvalue Env.t) Opt.t =
   match vstate with
   | Vabsent -> return (Vbool(false), Env.empty)
   | Vpresent(v) ->
-     let* env = matcheq v p in
+     let* env = pmatcheq v p in
      return (Vbool(true), env)
   | _ -> none
   
