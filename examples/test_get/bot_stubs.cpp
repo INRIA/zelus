de1e55b57408caf08defe7646a5c7fc50905383c
beki <bereketngussie99@gmail.com>
2021-10-07 08:17:22 +0300

    Modifications were made such that the shared variables are stored in a hash table

--- examples/test_get/bot_stubs.cpp
+++ examples/test_get/bot_stubs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unordered_map>
 extern "C" {
  #include <stdio.h>
  #include <caml/mlvalues.h>
@@ -6,10 +7,7 @@ extern "C" {
  #include <caml/alloc.h>//to create structured ocaml objects (for e.g., to encode a cpp double into an ocaml value type)
  #include <string.h>
 }
-std::string keys[5]={""};
-float values[5]={0.0};
-std::string cmd="";
-float a=0.0;
+std::unordered_map<std::string, float> table;
 extern "C" value move_robot_cpp(value speed){
   CAMLparam1 (speed);
   CAMLreturn (Val_unit);
@@ -23,14 +21,7 @@ extern "C" value robot_store_c(value command, value magnitude){
   CAMLparam2 (command, magnitude);
   std::string key= String_val(command);
   float val= Double_val(magnitude);
-  //for(int i=0;i<sizeof(keys);i++){
-  //	if(keys[i]==""||keys[i]==key){
-  //		keys[i]=key;
-  //		values[i]= val;
-  //	}
-  //}
-  cmd=key;
-  a=val;
+  table[key]=val;
   //std::cout<<"Inside robot_store*->*"<<cmd;
   CAMLreturn (Val_unit);
 }
@@ -40,20 +31,10 @@ extern "C" value robot_get_cpp(value var){
   std::string key= String_val(var);
   //std::cout<< "Inside robot_get!<->"<<key;
   float val;
-  //for(int i=0;i<sizeof (keys);i++){
-	//if (keys[i]==""||keys[i]==key){
-//		val=values[i];
-//	}
-//	else if (i==(sizeof(keys)-1)){
-//		val=0.0;
-//	}
-  //}
-  if (key==cmd){
-  val =a;
-  }
-  else {
-  val =0.0;
-  }
+  if (table.find(key) == table.end())
+  	val=0.0;
+  else
+  	val=table[key];
   v_res= caml_copy_double(val);
   CAMLreturn (v_res);
 }
