90ef6582cc0e0b6ec0a8bbfc9101997e3beaa4b6
jlvargasme <jlvm.ind@hotmail.com>
2021-10-13 17:49:50 -0400

    Adding expression parsing for expression validation in Z3

--- compiler/verif/z3verif.ml
+++ compiler/verif/z3verif.ml
@@ -21,6 +21,14 @@ exception Z3FailedException of string
 
 (*Open Zelus Interface*)
 open Zelus
+open Zident
+open Global
+open Modules
+open Zelus
+open Deftypes
+open Ztypes
+open Typerrors
+
 
 (*Define refinement type pattern*)
 type z3op =
@@ -30,14 +38,72 @@ type z3exp =
     | Z3int of int
     | Z3real of float
 
-let parse_expression e = e.e_desc 
+(* convert immeiate type to respective string *)
+let immediate = function
+  | Ebool(b) -> if b then "true" else "false" 
+  | Eint(i) -> Printf.sprintf "%d" i
+  | Efloat(i) -> Printf.sprintf "%f" i
+  | Echar(c) -> Printf.sprintf "'%c'" c
+  | Estring(c) -> Printf.sprintf "'%s'" c
+  | Evoid -> Printf.sprintf ""
+  
+let qualident { Lident.qual = m; Lident.id = s } =
+  Printf.sprintf "%s.%s" m s
+ 
+let print_elt f e = 
+	print_string (Printf.sprintf "List exp: '%s'\n" (f e)) 
+	
+(* parse input expression into string and redirect to Z3 solver *)
+let rec parse_expression e = 
+  match e.e_desc with
+    | Elocal n -> print_string "Elocal\n"; "1.0"
+    | Eglobal { lname = ln } -> print_string "Eglobal\n"; Lident.modname ln
+    | Eop(op, e_list) -> print_string "Eop\n"; "1.0"
+    | Elast x -> print_string "Elast\n"; "1.0"
+    | Econstr0(ln) -> print_string "Econstr0\n"; "1.0"
+    | Econst c -> print_string "Econst\n"; immediate c
+    | Eapp({ app_inline = i; app_statefull = r }, e, e_list) ->
+       print_string (Printf.sprintf "App inline: '%b'\n" i);
+       print_string (Printf.sprintf "App stateful: '%b'\n" r);
+       (*print_string (Printf.sprintf "Exp parsing: '%s' \n" (parse_expression e));
+       print_string "Map start\n";
+       let dummy = List.map (print_elt parse_expression) e_list in
+       print_string "Eapp\n"; "1.0"*)
+       let operator = Printf.sprintf "'%s'" (parse_expression e) in
+       let exp_list = List.map parse_expression e_list in
+       List.hd (List.tl exp_list)
+    | Econstr1(ln, e_list) ->
+        print_string "Econstr1\n"; "1.0"
+    | Etuple(e_list) ->
+        print_string "Etuple\n"; "1.0"
+    | Erecord_access(e, field) ->
+        print_string "Erecord_access\n"; "1.0"
+    | Erecord(ln_e_list) ->
+        print_string "Erecord\n"; "1.0"
+    | Erecord_with(e, ln_e_list) ->
+       print_string "Erecord_with\n"; "1.0"
+    | Elet(l, e) ->
+        print_string "Elet\n"; "1.0"
+    | Eblock(b, e) ->
+       print_string "Eblock\n"; "1.0"
+    | Etypeconstraint(e, typ) ->
+        print_string "Etypeconstraint\n"; "1.0"
+    | Eseq(e1, e2) ->
+        print_string "Eseq\n"; "1.0"
+    | Eperiod(p) ->
+        print_string "Eperiod\n"; "1.0"
+    | Ematch(total, e, match_handler_list) ->
+        print_string "Ematch\n"; "1.0"
+    | Epresent(present_handler_list, opt_e) ->
+        print_string "Epresent\n"; "1.0" 
+     
 
 let evaluate name ty e1 e2 : bool =
 	Printf.printf "Running Zelus evaluation verifier \n";
 	let cfg = [("model", "true"); ("proof", "false")] in
 	let ctx = (mk_context cfg) in
 	let var = Real.mk_numeral_s ctx e2 in
-	let threshold = Real.mk_numeral_s ctx "0.0" in
+	let threshold = Real.mk_numeral_s ctx e1 in
 	let phi variable refinement =
 		Arithmetic.mk_ge ctx variable refinement in
 	let correct = (Boolean.mk_and ctx
@@ -52,4 +118,4 @@ let evaluate name ty e1 e2 : bool =
 	     
 let rec prove_satisfiability op : bool =
 	match op with
-	| Z3eval(var, ty, e1, e2) -> evaluate var ty e1 ("-1.0")
+	| Z3eval(var, ty, e1, e2) -> evaluate var ty (parse_expression e1) (parse_expression e2)
