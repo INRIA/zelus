536232f647fa6ef1c6130999a2c645d630754f02
jlvargasme <jlvm.ind@hotmail.com>
2021-11-03 19:35:48 -0400

    adding z3 files

---
+++ examples/my_examples/refine/sample.ml
@@ -0,0 +1,17 @@
+(* The Zelus compiler, version 2.1-dev
+  (2021-05-19-23:17) *)
+open Ztypes
+let pi = 3.14159
+
+let y0 = 4.
+
+type _main = unit
+
+let main  = 
+   let main_alloc _ = () in
+  let main_reset self  =
+    ((()):unit) in 
+  let main_step self () =
+    ((let _ = print_string "end" in
+      print_newline ()):unit) in
+  Node { alloc = main_alloc; reset = main_reset ; step = main_step }
