307ad54d7377e45634d49e2ca1349cef873983fb
beki <bereketngussie99@gmail.com>
2021-11-18 19:20:11 +0300

    the new keywords were implemented using hash table with LCM

--- examples/lcm_test/test_stubs.c
+++ examples/lcm_test/test_stubs.c
@@ -2,37 +2,35 @@
 #include <string.h>
 #include <caml/mlvalues.h>
 #include <caml/alloc.h>
-#include "hash_tbl.h"
-//#include <lcm/lcm.h>
+#include <lcm/lcm.h>
+#include "example_t.h"
 //#include "robot_store_t.h"
 #include <time.h>
 
 //#define MBOT_MOTOR_TRANSVERSE_CHANNEL "MBOT_MOTOR_TRANSVERSE"
 
 // Sends motor commands to the robot
-float a=5.0;
+float a=1.0;
 CAMLprim value 
 robot_get_cpp(value var)
 {
     //std::string key = String_val(var);
     //printf("Command: %s\n", (String_val(var)));
-    printf("Robot get %s\n", String_val(var));
-    a=get_value(String_val(var));
     //printf("%f", a);
     return caml_copy_double(a);
 }
-//CAMLparam1 (var);
-//CAMLlocal1 (v_res);
-//std::string key= String_val(var);
- //std::cout<< "Inside variable ! \n"<<key;
-//v_res= Val_int(a);
-//CAMLreturn (v_res);
 
 CAMLprim value
 move_robot_cpp(value speed){
+	lcm_t * lcm = lcm_create(NULL);
+	example_t cmd;
+	cmd.utime = (unsigned long)time(NULL);
+	cmd.trans_v = Int_val(speed)/100.0;
+	cmd.angular_v = 0.;
+	example_t_publish(lcm, "EXAMPLE", &cmd);
+	lcm_destroy(lcm);
 	return Val_unit;
 }
-
 
 CAMLprim value
 robot_store_c(value cmd,value mag){
@@ -40,13 +38,11 @@ robot_store_c(value cmd,value mag){
 }
 
 CAMLprim value
-robot_str_cpp(value cmd,value mag){
-	printf("Inside robot store");
-	insert_to_table(String_val(cmd), Double_val(mag));
+control_robot_c(value t_speed, value a_speed){
 	return Val_unit;
 }
 
 CAMLprim value
-control_robot_c(value t_speed, value a_speed){
+robot_str_cpp (value cmd, value mag){
 	return Val_unit;
 }
--- examples/lcm_test_get/bot_stubs.c
+++ examples/lcm_test_get/bot_stubs.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <caml/mlvalues.h>
+#include <caml/memory.h>
 #include <caml/alloc.h>
+#include <lcm/lcm.h>
+#include "robot_store_t.h"
 #include "hash_tbl.h"
-//#include <lcm/lcm.h>
-//#include "robot_store_t.h"
 #include <time.h>
 
 //#define MBOT_MOTOR_TRANSVERSE_CHANNEL "MBOT_MOTOR_TRANSVERSE"
 
 // Sends motor commands to the robot
-float a=5.0;
+float a;
+
 CAMLprim value 
 robot_get_cpp(value var)
 {
     //std::string key = String_val(var);
     //printf("Command: %s\n", (String_val(var)));
-    printf("Robot get %s\n", String_val(var));
     a=get_value(String_val(var));
     //printf("%f", a);
     return caml_copy_double(a);
 }
-//CAMLparam1 (var);
-//CAMLlocal1 (v_res);
-//std::string key= String_val(var);
- //std::cout<< "Inside variable ! \n"<<key;
-//v_res= Val_int(a);
-//CAMLreturn (v_res);
 
 CAMLprim value
 move_robot_cpp(value speed){
@@ -41,8 +36,15 @@ robot_store_c(value cmd,value mag){
 
 CAMLprim value
 robot_str_cpp(value cmd,value mag){
-	printf("Inside robot store");
-	insert_to_table(String_val(cmd), Double_val(mag));
+	insert_to_table(String_val(cmd), Double_val(mag));//Store the variable to the local hash table (it can also be an update to an existing variable)
+	lcm_t * lcm = lcm_create(NULL);
+	robot_store_t my_cmd;
+	my_cmd.utime = (unsigned long)time(NULL);
+    // Need to divide by 100 and convert to float since motor speeds come in as hundredths of units (and as integer arguments)
+        my_cmd.command = String_val(cmd);
+        my_cmd.value = Double_val(mag);
+   	robot_store_t_publish(lcm, "MBOT_MOTOR_TRANSVERSE", &my_cmd); 
+   	lcm_destroy(lcm);
 	return Val_unit;
 }
 
