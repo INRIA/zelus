35139be81dba7b4c3dda92385acaecf41f3cabb8
Marc Pouzet <you@example.com>
2021-12-08 23:43:12 +0100

    Reorganisation.

--- zrun/eval.ml
+++ zrun/eval.ml
@@ -11,297 +11,116 @@
 (*                                                                     *)
 (* *********************************************************************)
 
-(* Evaluation of combinatorial expressions. The input environment *)
-(* is only made of primitive values *)
-(* The presence of a stateful construct leads to an error *)
+(* the main *)
+open Misc
 open Smisc
 open Monad
-open Opt                                                            
-open Ident
-open Zelus
+open Opt
 open Value
+open Lident
 open Primitives
-open Sdebug
+open Coiteration
+open Location
+               
+let lexical_error err loc =
+  Format.eprintf "%aIllegal character.@." output_location loc;
+  raise Stdlib.Exit
 
-let find_value_opt x env =
-  let* v = Env.find_opt x env in
-  match v with
-  | Vbot | Vnil -> none
-  | Value(v) -> return v
-      
-let find_gvalue_opt x env =
+let syntax_error loc =
+  Format.eprintf "%aSyntax error.@." output_location loc;
+  raise Stdlib.Exit
+
+let parse parsing_fun lexing_fun source_name =
+  let ic = open_in source_name in
+  let lexbuf = Lexing.from_channel ic in
+  lexbuf.Lexing.lex_curr_p <-
+    { lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = source_name };
   try
-    let { Global.info } = Genv.find x env in
-    return info
+    parsing_fun lexing_fun lexbuf
   with
-  | Not_found -> none
-
-(* merge two environments provided they do not overlap *)
-let merge env1 env2 =
-  let s = Env.to_seq env1 in
-  seqfold
-    (fun acc (x, entry) ->
-      if Env.mem x env2 then none
-      else return (Env.add x entry acc))
-    env2 s
-
-
-(* value of an immediate constant *)
-let immediate v =
-  match v with
-  | Eint(v) -> Vint(v)
-  | Ebool(b) -> Vbool(b)
-  | Evoid -> Vvoid
-  | Efloat(f) -> Vfloat(f)
-  | Echar(c) -> Vchar(c)
-  | Estring(s) -> Vstring(s)
-                
-(* evaluation function *)
-let rec exp genv env { e_desc; e_loc } =
-  let r = match e_desc with   
-    | Econst(v) ->
-       return (immediate v)
-    | Econstr0 { lname } ->
-       return (Vconstr0(lname))
-    | Elocal x ->
-       let* v =
-         find_value_opt x env |>
-           Error.error e_loc (Error.Eunbound_ident(x)) in
-       return v
-    | Eglobal { lname } ->
-       let* v =
-         find_gvalue_opt lname genv |>
-           Error.error e_loc (Error.Eunbound_lident(lname)) in
-       return v
-    | Eop(op, e_list) ->
-       begin match op, e_list with
-       | Eifthenelse, [e1; e2; e3] ->
-          let* v = exp genv env e1 in
-          let* v = bool v in
-          if v then exp genv env e2
-          else exp genv env e3
-       | _ -> none
-       end
-    | Econstr1 { lname; arg_list } ->
-       let* v_list = exp_list genv env arg_list in
-       return (Vconstr1(lname, v_list))
-    | Etuple(e_list) ->
-       let* v_list = exp_list genv env e_list in
-       return (Vstuple(v_list))
-    | Eapp(f, e_list) ->
-       let* v = exp genv env f in
-       apply genv env v e_list
-    | Elet({ l_rec; l_eq }, e) ->
-       if l_rec then none
-       else
-         let* l_env = eval_eq genv env l_eq in
-         exp genv (Env.append l_env env) e
-    | Ematch { e; handlers } ->
-       let* v = exp genv env e in
-       match_handlers genv env v handlers
-    | Erecord_access { label; arg } ->
-       let* arg = exp genv env arg in
-       record_access { label; arg }  
-    | Erecord(r_list) ->
-       let* r_list =
-         Opt.map
-           (fun { label; arg } ->
-             let* arg = exp genv env arg in return ({ label; arg }))
-           r_list in
-       return (Vrecord(r_list))
-    | Erecord_with(r, r_list) ->
-       let* r = exp genv env r in
-       let* label_arg_list = get_record r in
-       let* label_arg_list' =
-         Opt.map
-           (fun { label; arg } ->
-             let* arg = exp genv env arg in return ({ label; arg }))
-           r_list in
-       let* r = record_with label_arg_list label_arg_list in
-       return (Vrecord(r))
-    | Etypeconstraint(e, _) -> exp genv env e
-    | Efun(fe) ->
-       funexp genv env fe
-    | Epresent _ -> none
-    | Ereset(e_body, e_res) ->
-       let* v = exp genv env e_res in
-       let* _ = bool v in
-       exp genv env e_body
-    | Elast _ -> none
-    | Eassert(e_body) ->
-       let* v = exp genv env e_body in
-       let* v = bool v in
-       (* stop when [no_assert = true] *)
-       if !no_assert then return (Vvoid)
-       else if v then return (Vvoid) else none in
-  Error.stop_at_location e_loc r
-
-and exp_list genv env e_list = Opt.map (exp genv env) e_list
-
-and record_access { label; arg } =
-  (* look for [label] in the value of [arg] *)
-  let* record_list = get_record arg in
-  let rec find l record_list =
-    match record_list with
-    | [] -> none
-    | { label; arg } :: record_list ->
-       if label = l then return arg
-       else find l record_list in
-  find label record_list
-  
-and record_with label_arg_list ext_label_arg_list = 
-  (* inject {label; arg} into a record *)
-  let rec inject label_arg_list l arg =
-    match label_arg_list with
-    | [] -> none
-    | { label } as entry :: label_arg_list ->
-       if label = l then return ({ label; arg } :: label_arg_list)
-       else let* label_arg_list = inject label_arg_list l arg in
-            return (entry :: label_arg_list) in
-  (* join the two *)
-  let rec join label_arg_list ext_label_arg_list =
-    match ext_label_arg_list with
-    | [] -> return label_arg_list
-    | { label; arg } :: ext_label_arg_list ->
-       let* label_arg_list = inject label_arg_list label arg in
-       join label_arg_list ext_label_arg_list in
-  join label_arg_list ext_label_arg_list
-  
-(* application [fv e_list] *)
-and apply genv env fv e_list =
-  (* apply a closure to a list of arguments *)
-  let rec apply_closure genv env fe f_args e_list f_body =
-    match f_args, e_list with
-    | [], [] ->
-       result genv env f_body
-    | arg :: f_args, ({ e_loc } as e) :: e_list ->
-       let* v = exp genv env e in
-       let* env =
-         matching_arg_in env arg v |>
-           Error.error e_loc Error.Epattern_matching_failure in
-       apply_closure genv env fe f_args e_list f_body
-    | [], _ ->
-       let* fv = result genv env f_body in
-       apply genv env fv e_list
-    | _, [] ->
-       return (Vclosure({ fe with f_args = f_args }, genv, env)) in
-  (* main entry *)
+  | Lexer.Lexical_error(err, loc) ->
+     close_in ic; lexical_error err loc
+  | Parser.Error ->
+     close_in ic;
+     syntax_error
+       (Loc(Lexing.lexeme_start lexbuf, Lexing.lexeme_end lexbuf))
+     
+let parse_implementation_file source_name =
+  parse Parser.implementation_file Lexer.main source_name
+               
+(* The main entry function. Execute [n] steps of [main()] or [run main ()]  *)
+let eval_main genv n_steps is_check main =
+  let fv =
+    find_gvalue_opt (Name main) genv in
   match fv with
-  | Vfun(op) ->
-     let* fv =
-       match e_list with
-       | [] -> (* typing error *) none
-       | e :: e_list ->
-          let* v = exp genv env e in
-          let* fv = op v in apply genv env fv e_list in
-     return fv
-  | Vclosure(({ f_kind = (Kstatic | Kfun);
-                f_args; f_body } as fe, genv, env)) ->
-     apply_closure genv env fe f_args e_list f_body
-  | _ ->
-     return fv
-      
-(* step function for an equation *)
-and eval_eq genv env { eq_desc; eq_loc } =
-  let r = match eq_desc with 
-    | EQeq(p, e) -> 
-       let* v = exp genv env e in
-       let* env_p1 =
-         Match.pmatcheq v p |>
-           Error.error eq_loc Error.Epattern_matching_failure in
-       let env_p1 = Env.map (fun v -> Value(v)) env_p1 in
-       return env_p1
-    | EQif(e, eq1, eq2) ->
-       let* v = exp genv env e in
-       let* v = bool v in
-       if v then
-         let* env1 = eval_eq genv env eq1 in
-         return env1
-       else
-         let* env2 = eval_eq genv env eq2 in
-         return env2
-    | EQand(eq_list) ->
-       let and_eq env acc eq =
-         let* env_eq = eval_eq genv env eq in
-         let* acc = merge env_eq acc in
-         return acc in
-       let* env_eq = fold (and_eq env) Env.empty eq_list in
-       return env_eq
-    | EQreset(eq, e) -> 
-       let* v = exp genv env e in 
-       let* _ = bool v in
-       eval_eq genv env eq
-    | EQempty -> return Env.empty
-    | EQassert(e) ->
-       let* v = exp genv env e in
-       let* v = bool v in
-       (* stop when [no_assert = true] *)
-       if !no_assert then return Env.empty
-       else if v then return Env.empty else none
-    | EQlet({ l_rec; l_eq }, eq_let) ->
-       if l_rec then none
-       else
-         let* l_env = eval_eq genv env l_eq in
-         eval_eq genv (Env.append l_env env) eq_let
-    | EQder _ | EQpresent _ | EQinit _
-      | EQemit _ | EQlocal _ | EQautomaton _ | EQmatch _ -> none in
-  Error.stop_at_location eq_loc r
-  
-and match_handlers genv env v handlers =
-  match handlers with
-  | [] -> none
-  | { m_pat; m_body } :: handlers ->
-     let r = Match.pmatch v m_pat in
-     match r with
+  | None ->
+     Format.eprintf "The global value %s is unbound.\n" main;
+     raise Stdlib.Exit
+  | Some(fv) ->
+     let v = to_fun fv in
+     match v with
      | None ->
-        (* this is not the good handler; try an other one *)
-        match_handlers genv env v handlers
-     | Some(env_pat) ->
-        let env_pat = Env.map (fun v -> Value(v)) env_pat in
-        let env = Env.append env_pat env in
-        exp genv env m_body
-        
-and funexp genv env fe = return (Vclosure(fe, genv, env))
+        Format.eprintf "The global value %s is not a function.\n" main;
+        raise Stdlib.Exit
+     | Some(v) ->
+        let output =
+          if is_check then
+            fun v -> if (v = Vbool(true)) then () else raise Stdlib.Exit
+          else
+            fun v -> Output.pvalue_flush Format.std_formatter v in
+        match v with
+        | Vfun(fv) ->
+           run_fun output fv n_steps
+        | Vnode { init; step } ->
+           run_node output init step n_steps
+        | _ -> assert false
+             
+let apply_with_close_out f o =
+  try
+    f o;
+    close_out o
+  with x -> close_out o; raise x
 
-and block genv env { b_vars; b_body; b_loc } =
-  (* a very simple solution. no fixpoint *)
-  let r =
-    let* _ =
-      Opt.fold
-        (fun acc { var_name; var_default; var_init } ->
-          match var_default, var_init with
-          | (None, None) -> return (S.add var_name acc)
-          | _ -> none (* type error *)) S.empty b_vars in
-    let* b_env = eval_eq genv env b_body in
-    return (Env.append b_env env, b_env) in
-  Error.stop_at_location b_loc r
+let do_step comment step input = 
+  let output = step input in
+  Debug.print_message comment;
+  Debug.print_program output;
+  output
 
-and result genv env { r_desc; r_loc } =
-  let r = match r_desc with
-    | Exp(e) -> exp genv env e
-    | Returns(b) ->
-       let* env, _ = block genv env b in
-       let* v = matching_arg_out env b in
-       return v in
-  Error.stop_at_location r_loc r
+(* Evaluate all the definition in a file, store values *)
+(* and a main function/node, if given *)
+let file modname filename n_steps is_check main_nodes =
+  (* output file in which values are stored *)
+  let obj_name = filename ^ ".zlo" in
+  let otc = open_out_bin obj_name in
+    
+  let source_name = filename ^ ".zls" in
 
-and matching_arg_out env { b_vars; b_loc } =
-  let r =
-    let* v_list =
-      Opt.map
-        (fun { var_name } -> find_value_opt var_name env) b_vars in
-    match v_list with
-    | [] -> return (Vvoid)
-    | [v] -> return v
-    | _ -> return (Vstuple(v_list)) in
-  Error.stop_at_location b_loc r
+  Location.initialize source_name;
+
+  (* Parsing *)
+  let impl_list = parse_implementation_file source_name in
+  Debug.print_message "Parsing done";
+
+  (* Scoping *)
+  let impl_list = Scoping.program impl_list in
+  Debug.print_message "Scoping done";
   
-and matching_arg_in env arg v =
-  let match_in acc { var_name } v =
-    return (Env.add var_name (Value(v)) acc) in
-  match arg, v with
-  | [], Vvoid -> return env
-  | l, Vstuple(v_list) -> 
-     Opt.fold2 match_in env l v_list
-  | [x], _ -> match_in Env.empty x v
-  | _ -> none
+  (* Write defined variables for equations *)
+  let impl_list = do_step "Write done" Write.program impl_list in
+
+  (* Evaluation of definitions in [filename] *)
+  let genv = Genv.initialize modname [] in
+  (* Add Stdlib *)
+  let genv = Genv.add_module genv Primitives.stdlib_env in
+  
+  let genv = Opt.get (Coiteration.program genv impl_list) in
+  Debug.print_message "Evaluation of definitions done";
+
+  (* Write the values into a file *)
+  (* Genv.write genv otc; *)
+  apply_with_close_out (Genv.write genv) otc;
+  
+  (* evaluate a list of main function/nodes *)
+  List.iter (eval_main genv n_steps is_check) main_nodes
+             
--- zrun/zrun.ml
+++ zrun/zrun.ml
@@ -12,125 +12,15 @@
 (* *********************************************************************)
 
 (* the main *)
-open Misc
 open Smisc
-open Monad
-open Opt
-open Value
-open Lident
-open Primitives
-open Coiteration
-open Location
-               
-let lexical_error err loc =
-  Format.eprintf "%aIllegal character.@." output_location loc;
-  raise Stdlib.Exit
-
-let syntax_error loc =
-  Format.eprintf "%aSyntax error.@." output_location loc;
-  raise Stdlib.Exit
-
-let parse parsing_fun lexing_fun source_name =
-  let ic = open_in source_name in
-  let lexbuf = Lexing.from_channel ic in
-  lexbuf.Lexing.lex_curr_p <-
-    { lexbuf.Lexing.lex_curr_p with Lexing.pos_fname = source_name };
-  try
-    parsing_fun lexing_fun lexbuf
-  with
-  | Lexer.Lexical_error(err, loc) ->
-     close_in ic; lexical_error err loc
-  | Parser.Error ->
-     close_in ic;
-     syntax_error
-       (Loc(Lexing.lexeme_start lexbuf, Lexing.lexeme_end lexbuf))
-     
-let parse_implementation_file source_name =
-  parse Parser.implementation_file Lexer.main source_name
-               
-(* The main entry function. Execute [n] steps of [main()] or [run main ()]  *)
-let eval_main genv n_steps is_check main =
-  let fv =
-    find_gvalue_opt (Name main) genv in
-  match fv with
-  | None ->
-     Format.eprintf "The global value %s is unbound.\n" main;
-     raise Stdlib.Exit
-  | Some(fv) ->
-     let v = to_fun fv in
-     match v with
-     | None ->
-        Format.eprintf "The global value %s is not a function.\n" main;
-        raise Stdlib.Exit
-     | Some(v) ->
-        let output =
-          if is_check then
-            fun v -> if (v = Vbool(true)) then () else raise Stdlib.Exit
-          else
-            fun v -> Output.pvalue_flush Format.std_formatter v in
-        match v with
-        | Vfun(fv) ->
-           run_fun output fv n_steps
-        | Vnode { init; step } ->
-           run_node output init step n_steps
-        | _ -> assert false
-             
-let apply_with_close_out f o =
-  try
-    f o;
-    close_out o
-  with x -> close_out o; raise x
-
-let do_step comment step input = 
-  let output = step input in
-  Debug.print_message comment;
-  Debug.print_program output;
-  output
-
-(* Evaluate all the definition in a file, store values *)
-(* and a main function/node, if given *)
-let eval_file modname filename suffix n_steps is_check main_nodes =
-  (* output file in which values are stored *)
-  let obj_name = filename ^ ".zlo" in
-  let otc = open_out_bin obj_name in
-    
-  let source_name = filename ^ suffix in
-
-  Location.initialize source_name;
-
-  (* Parsing *)
-  let impl_list = parse_implementation_file source_name in
-  Debug.print_message "Parsing done";
-
-  (* Scoping *)
-  let impl_list = Scoping.program impl_list in
-  Debug.print_message "Scoping done";
-  
-  (* Write defined variables for equations *)
-  let impl_list = do_step "Write done" Write.program impl_list in
-
-  (* Evaluation of definitions in [filename] *)
-  let genv = Genv.initialize modname [] in
-  (* Add Stdlib *)
-  let genv = Genv.add_module genv Primitives.stdlib_env in
-  
-  let genv = Opt.get (Coiteration.program genv impl_list) in
-  Debug.print_message "Evaluation of definitions done";
-
-  (* Write the values into a file *)
-  (* Genv.write genv otc; *)
-  apply_with_close_out (Genv.write genv) otc;
-  
-  (* evaluate a list of main function/nodes *)
-  List.iter (eval_main genv n_steps is_check) main_nodes
-             
+   
 let main file =
   if Filename.check_suffix file ".zls"
   then 
     let filename = Filename.chop_extension file in
     let modname = String.capitalize_ascii (Filename.basename filename) in
-    eval_file modname
-      filename ".zls" !Smisc.number_of_steps !Smisc.set_check !Smisc.main_nodes 
+    Eval.file modname
+      filename !Smisc.number_of_steps !Smisc.set_check !Smisc.main_nodes 
   else raise (Arg.Bad "Expected *.zls file.")
                                                        
 let doc_main = "\tThe main node to evaluate"
