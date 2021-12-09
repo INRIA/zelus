81c08ecc490c51fc7c2e72085abb45c48a7751dc
Marc Pouzet <you@example.com>
2021-12-09 10:54:08 +0100

    --

--- test/test_runner.ml
+++ test/test_runner.ml
@@ -1,3 +1,17 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                                                     *)
+(*          Zelus, a synchronous language for hybrid systems           *)
+(*                                                                     *)
+(*  (c) 2021 Inria Paris (see the AUTHORS file)                        *)
+(*                                                                     *)
+(*  Copyright Institut National de Recherche en Informatique et en     *)
+(*  Automatique. All rights reserved. This file is distributed under   *)
+(*  the terms of the INRIA Non-Commercial License Agreement (see the   *)
+(*  LICENSE file).                                                     *)
+(*                                                                     *)
+(* *********************************************************************)
+
 open Misc
 open Initial
 open Compiler
