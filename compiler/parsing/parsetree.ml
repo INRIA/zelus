914ec910d10f11bb9e0dcac828557fa0656a19c6
Marc Pouzet <you@example.com>
2022-07-19 14:44:14 +0200

    New syntax for outputs in for loops.

--- compiler/parsing/parsetree.ml
+++ compiler/parsing/parsetree.ml
@@ -250,12 +250,9 @@ and for_index_desc =
   (* i in e1..e2; [e1] and [e2] must be sizes *)
 
 and for_out_desc = 
-  | Earray :
-      (* [xi out x] means that the output is the array x st x.(i) = xi *)
-      { xi : name; x: name } -> for_out_desc
-  | Eaccumulate :
-      (* [xi init v] means that the output is the cumulated value of xi *)
-      { xi : name; init : exp } -> for_out_desc
+  { xi : exp vardec; (* [xi [init e] [default e]] [out x] *)
+    x : name option; (* [xi out x] *)
+  }
 
 and 'body escape_desc =
   { e_cond: scondpat; 
