450733538354a25573bbaa98eb8f97265e604842
Marc Pouzet <Marc.Pouzet@ens.fr>
2024-11-28 09:22:04 +0100

    Update t_3.ml

--- src/ctests/good/t_3.ml
+++ src/ctests/good/t_3.ml
@@ -11,12 +11,11 @@ let m = let machine_18  =
               i_17 = (false:bool); m_16 = (42:int); o_10 = (42:int) } in
           let machine_18_reset self  =
             ((self.i_17 <- true):unit) in
-          let machine_18_step self ((m_13:Stdlib.int)) =
-            ((self.o_10 <- (if i_17 then 0 else Stdlib.(+) m_16 m_13);
-              self.i_17 <- false; self.m_16 <- Stdlib.(+) o_10 1; self.o_10):
+          let machine_18_step self ((m_13 : int)) =
+            ((self.o_10 <- (if self.i_17 then 0 else Stdlib.(+) self.m_16 m_13);
+              self.i_17 <- false; self.m_16 <- Stdlib.(+) self.o_10 1; self.o_10):
             int) in
           Node { alloc = machine_18_alloc; reset = machine_18_reset;
                                            step = machine_18_step } in
           machine_18
 
-let m = T_3.r_7
