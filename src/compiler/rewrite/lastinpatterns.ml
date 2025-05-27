f00b7ceb3e3eac3bcfcda6a85e274aea9591186a
Marc Pouzet <Marc.Pouzet@ens.fr>
2025-05-27 16:06:43 +0200

    Update lastinpatterns.ml

--- src/compiler/rewrite/lastinpatterns.ml
+++ src/compiler/rewrite/lastinpatterns.ml
@@ -20,94 +20,95 @@
 (* [local m do m = x and eq] *)
 
 (* Example:
- *- [let node f(x) = ... last x...] is rewritten
- *- [let node f(m) = local x do x = m and ... last* x...]
+ *- [let node f(x) = ... x ... last x...] is rewritten
+ *- [let node f(m) = local last x do x = m and ... m ... last* x...]
 
  *- [let node f(...x init e1 default e0...) returns (...) ...last x] is rewritten
  *- [let node f(...m...) returns (...)
- *-       local x init ... default ... do x = m and ...last* x done]
+ *-       local x init ... default ... do x = m and ...last x done]
 
- *- [let node f(...) returns (...x init ... default ...) ... last x ...] is rewritten
+ *- [let node f(...) returns (...x init ... default ...) ... last* x ...] is rewritten
  *- [let node f(...) returns (...m ...)
-       local x init ... default ... do m = x and ... last* x]
+       local x init ... default ... do x = m and ... last* x]
 
  *- [match e with P(...x...) -> eq] is rewritten
- *- [match e with P(...m...) -> 
-       local x do x = m and eq[last* x \ last x]]
+ *- [match e with P(...m...) -> local x do x = m and eq]
  *- [present
        e(...x...) -> eq...[last x]...] is rewritten
  *- [present
-       e(...m...) ->
-         local x do x = m and eq[last* x \ last x]]
+       e(...m...) -> local x do x = m and eq]
  *)
 
            
 open Zelus
 open Ident
 
-type acc = entry Env.t
-
-and entry = { last_is_used: bool; new_name: Ident.t }
+(* The accumulator is a map : [name -> name]. It associates a fresh *)
+(* name [m] to every variable [x] for which [last x] is used *)
+type acc = Ident.t Env.t
 
 let empty = Env.empty
+
+let build funs acc l_env =
+  (* if [last x] is used introduce a fresh name [m] to store its result *)
+  let add x { Deftypes.t_sort } acc =
+    match t_sort with
+    | Sort_mem { m_last = true } | Sort_mem { m_init = Decl _ }
+      | Sort_mem { m_default = Decl _ } ->
+       let m = fresh "m" in Env.add x m acc
+    | _ -> acc in
+  Env.fold add l_env acc
 
 let new_vardec v =
   { v with var_default = None; var_init = None;
            var_clock = false; var_typeconstraint = None;
            var_is_last = false; var_init_in_eq = false }
 
+(* update a variable declaration; remove the initialization *)
+(* and default part *)
 let update_vardec acc ({ var_name } as v) =
+  try
+    let m = Env.find var_name acc in
+    { (new_vardec v) with var_name = m }
+  with
+  | Not_found -> v
+
+(*
+  let update_vardec acc ({ var_name } as v) =
   try
     let { new_name } = Env.find var_name acc in
     { (new_vardec v) with var_name = new_name }
   with
-    | Not_found -> v
-
-let build funs acc l_env =
-  let add x entry (new_env, acc) =
-    let m = fresh "m" in
-    Env.add m entry new_env,
-    Env.add x { last_is_used = false; new_name = m } acc in
-  Env.fold add l_env (Env.empty, acc)
+    | Not_found -> v *)
 
 let var_ident funs acc x =
   try
-    let { new_name } = Env.find x acc in new_name, acc
+    let m = Env.find x acc in m, acc
   with
   | Not_found -> x, acc
 
-(* when [last x] appears, it is replaced by [last* x] and [x] is marked to *)
-(* be used in [acc] *)
+(* when [last x] appears, it is replaced by [last* x] *)
 let last_ident funs acc ({ copy; id } as l) =
   try
-    let { last_is_used; new_name } = Env.find id acc in
-    let acc = if last_is_used then acc
-              else
-                Env.update id
-                  (function | None -> None
-                            | Some(z) -> Some { z with last_is_used = true })
-                  acc in
-    { l with copy = false }, acc
+    let _ = Env.find id acc in { l with copy = false }, acc
   with
   | Not_found -> l, acc
 
-(* if [acc(x) = { last_is_used; new_name = m }] add equation [x = m] *)
-let add acc x _ (v_list, eq_list) =
+(* if [acc(x) = m] add the equation [x = m] to [eq_list] *)
+let add_x_m acc x _ (v_list, eq_list) =
   try
-    let { last_is_used; new_name } = Env.find x acc in
-    if last_is_used then
-      Aux.vardec x false None None :: v_list,
-      Aux.id_eq x (Aux.var new_name) :: eq_list
-    else v_list, eq_list
+    let m = Env.find x acc in
+    Aux.vardec x false None None :: v_list,
+    Aux.id_eq x (Aux.var m) :: eq_list
   with
   | Not_found -> v_list, eq_list
 
 let add_equations_into_eq acc env eq =
-  let v_list, eq_list = Env.fold (add acc) env ([], []) in
+  let v_list, eq_list = Env.fold (add_x_m acc) env ([], []) in
   Aux.eq_local_vardec v_list (eq :: eq_list)
 
 let add_equations_into_e acc env e =
-  let v_list, eq_list = Env.fold (add acc) env ([], []) in
+  let v_list, eq_list = Env.fold (add_x_m acc) env ([], []) in
   Aux.e_local_vardec v_list eq_list e
 
 let match_handler_eq funs acc m_h =
@@ -149,27 +150,25 @@ let block funs acc ({ b_vars; b_body; b_write } as b) =
   let b_body, acc = Mapfold.equation_it funs acc b_body in
   { b with b_vars; b_body }, acc
 
+(* update the list of arguments of a function *)
+(* such that default and initial values are removed *)
 let update_arg_list acc (v_list, eq_list) f_args =
-  let update_vardec acc (v_list, eq_list)
+  let update_vardec (v_list, eq_list)
         ({ var_name; var_init; var_default; var_is_last; var_init_in_eq } as v) =
-    let is_none = function | None -> true | _ -> false in
-    let i = (is_none var_init) && (is_none var_default)
-            && not var_is_last && not var_init_in_eq in
-    let { last_is_used; new_name } =
-      try Env.find var_name acc with | Not_found -> assert false in
-    { (new_vardec v) with var_name = new_name },
-    if last_is_used || not i then
-      v :: v_list, Aux.id_eq var_name (Aux.var new_name) :: eq_list
-    else v_list, eq_list in
-
-  Util.mapfold (fun (v_list, eq_list) arg ->
-      Util.mapfold (update_vardec acc) (v_list, eq_list) arg) ([], []) f_args
+    try
+      let m = Env.find var_name acc in
+      { (new_vardec v) with var_name = m } :: v_list,
+      Aux.id_eq var_name (Aux.var m) :: eq_list
+    with
+    | Not_found -> v :: v_list, eq_list in
+  Util.mapfold update_vardec (v_list, eq_list) f_args
 
 let funexp funs acc ({ f_args; f_body = ({ r_desc } as r); f_env } as f) =
   let arg acc v_list = Util.mapfold (Mapfold.vardec_it funs) acc v_list in
-  let f_args, acc = Util.mapfold arg acc f_args in
 
   let f_env, acc = Mapfold.build_it funs.global_funs acc f_env in
+
+  let f_args, acc = Util.mapfold arg acc f_args in
 
   let f_args, r_desc, acc = match r_desc with
     | Exp(e) ->
