33f0fc2f3596a3af794b9a7e65b3449ce95687b3
Marc Pouzet <Marc.Pouzet@ens.fr>
2025-07-05 16:25:09 +0200

    Update

--- src/compiler/common/aux.ml
+++ src/compiler/common/aux.ml
@@ -245,8 +245,8 @@ let e_local b e = emake (Elocal(b, e))
 
 let e_local_vardec vardec_list eq_list e =
   match vardec_list, eq_list with
-  | [], _ -> e_letrec eq_list e
   | (_, []) -> e
+  | [], _ -> e_letrec eq_list e
   | _ -> e_local (block_make vardec_list eq_list) e
 
 let eq_ifthen e eq_true =
