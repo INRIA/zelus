0e67bae18c95291deb5cd6a0bef9187e115c0906
Marc Pouzet <Marc.Pouzet@ens.fr>
2025-07-17 18:08:05 +0200

    Update global.ml
    Add field f_kind.

--- src/compiler/typdefs/global.ml
+++ src/compiler/typdefs/global.ml
@@ -49,7 +49,8 @@ and vexp =
       }
 
 and funexp =
-  { f_args: Typinfo.arg list; f_body: Typinfo.result; f_env: Typinfo.ienv Env.t }
+  { f_args: Typinfo.arg list; f_kind: Zelus.kind;
+    f_body: Typinfo.result; f_env: Typinfo.ienv Env.t }
 
 (* Value constructors *)
 type constr_desc =
