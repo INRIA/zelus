dcd2be6bfd53d91840f18377aa700c03ddbe4bba
Marc Pouzet <Marc.Pouzet@ens.fr>
2025-07-18 22:50:20 +0200

    Update copy.ml
    - Simple copy elimination.
    - Only propagate [x = y] definitions that appear in non recursive definition.

--- src/compiler/rewrite/copy.ml
+++ src/compiler/rewrite/copy.ml
@@ -3,7 +3,7 @@
 (*                                                                     *)
 (*          Zelus, a synchronous language for hybrid systems           *)
 (*                                                                     *)
-(*  (c) 2024 Inria Paris (see the AUTHORS file)                        *)
+(*  (c) 2025 Inria Paris (see the AUTHORS file)                        *)
 (*                                                                     *)
 (*  Copyright Institut National de Recherche en Informatique et en     *)
 (*  Automatique. All rights reserved. This file is distributed under   *)
@@ -12,104 +12,50 @@
 (*                                                                     *)
 (* *********************************************************************)
 
-(* Elimination of atomic values copy variables [x = y], constants, globals *)
-(* The transformation is applied after static scheduling and before *)
-(* translation into sequential code *)
+(* Elimination of copy variables [x = y] defined in non recursive let/in *)
 
 open Misc
 open Ident
 open Zelus
 open Deftypes
 
-(** atomic expressions - either immediate constants or variables *)
-type 'a value = | Vvar of Ident.t | Vlast of Zelus.last | Vconst of 'a
-						    
-type 'a renaming =
-    { rel: 'a Env.t; (* the substitution *)
-      defs: S.t; (* names for which the substitution cannot be applied *)
-    }
+(* a map: ident -> ident *)
+(* bounded names are renamed; otherwise, they stay unchanged *)
+type acc = Ident.t Env.t
 
-let empty = { rel = Env.empty; defs = S.empty }
+let empty = Env.empty
 
-(** Append a renaming with a new relation *)
-let append new_rel ({ rel } as renaming) =
-  { renaming with rel = Env.append new_rel rel }
-    
-(* Apply the renaming recursively. If [rel = [n\n1, n1\n2]], then *)
-(* [rename n rel] = n2 *)
-(* A substitution [n\last m] is not performed when [m] belongs to [defs] *)
-let rename n { rel; defs } =
-  let rec rename n =
-    try 
-      let m = Env.find n rel in
-      begin
-	match m with
-	| Vvar m -> rename m
-	| Vlast({ id } as l) ->
-           if S.mem id defs then raise Not_found else Elast l
-	| Vconst(v) -> v
-      end
-    with Not_found -> Evar n in
-  rename n
-
-let rec size ({ rel } as renaming) ({ desc } as s) =
-  try
-    let s =
-      match desc with
-      | Size_int _ -> s
-      | Size_var(n) ->
-         let n = Env.find n rel in
-         begin match n with
-         | Vvar n -> { s with desc = Size_var(n) }
-         | _ -> raise Not_found
-         end
-      | Size_op(op, s1, s2) ->
-         { s with desc = Size_op(op, size renaming s1, size renaming s2) }
-      | Size_frac({ num } as f) ->
-         { s with desc = Size_frac({ f with num = size renaming num }) } in
-    s
-  with
-    Not_found -> s
-
-(* Build a substitution [x1\v1,...,xn\vn] *)
-let rec build rel { eq_desc } =
+let rec remove_copies acc ({ eq_desc; eq_loc } as eq) =
   match eq_desc with
-  | EQeq({ pat_desc = Evarpat(x) }, { e_desc }) ->
-     let rel =
-       match e_desc with
-       | Evar m -> Env.add x (Vvar(m)) rel
-       | Eglobal _ | Econst _ -> Env.add x (Vconst(e_desc)) rel
-       | Elast m -> Env.add x (Vlast(m)) rel
-       | _ -> rel in
-     rel
-  | EQreset(eq, _) -> build rel eq
-  | EQand { eq_list } -> List.fold_left build rel eq_list
-  | _ -> rel
-
-(* Function to traverse the ast *)
-(* Apply [renaming] to every sub-expression *)
-let expression funs renaming ({ e_desc } as e) =
+  | EQeq({ pat_desc = Evarpat(x) }, { e_desc = Evar(y) }) ->
+     { eq with eq_desc = EQempty }, Env.add x y acc
+  | EQand { ordered; eq_list } ->
+     let eq_list, acc = 
+       Util.mapfold remove_copies acc eq_list in
+     Aux.set_loc_if_not_empty eq_loc (Aux.par_t ordered eq_list), acc
+  | _ -> raise Mapfold.Fallback
+     
+let expression funs acc ({ e_desc; e_loc } as e) =
   match e_desc with
-  | Evar(id) -> { e with e_desc = rename id renaming }, renaming
+  | Evar(x) ->
+     let x = try Env.find x acc with Not_found -> x in 
+     { e with e_desc = Evar(x) }, acc 
+  | Elet(leq, e_let) ->
+     let leq, acc = Mapfold.leq_it funs acc leq in
+     let e_let, acc = Mapfold.expression_it funs acc e_let in
+     { (Aux.e_let leq e_let) with e_loc }, acc
   | _ -> raise Mapfold.Fallback
 						
 (* Local declarations *)
-let leq_t funs renaming ({ l_eq } as l) =
-  let rel = build Env.empty l_eq in
-  let new_renaming = append rel renaming in
-  let l_eq, renaming = Mapfold.equation_it funs new_renaming l_eq in
-  { l with l_eq }, renaming
-
-let block funs renaming ({ b_body } as b) =
-  let rel = build Env.empty b_body in
-  let new_renaming = append rel renaming in
-  let b_body, renaming = Mapfold.equation_it funs new_renaming b_body in
-  { b with b_body }, renaming
+let leq_t funs acc ({ l_rec; l_eq } as l) =
+  let l_eq, acc = Mapfold.equation_it funs acc l_eq in
+  let l_eq, acc = if l_rec then l_eq, acc else remove_copies acc l_eq in
+  { l with l_eq }, acc
 
 let program genv p =
   let global_funs = Mapfold.default_global_funs in
   let funs =
     { Mapfold.defaults with
-      expression; leq_t; block; global_funs } in
+      expression; leq_t; global_funs } in
   let p, _ = Mapfold.program_it funs empty p in
   p
