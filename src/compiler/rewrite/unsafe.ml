8cd154492583982653f2493b3923a9e3e78afe0e
Marc Pouzet <Marc.Pouzet@ens.fr>
2025-07-21 09:28:14 +0200

    Update

--- src/compiler/rewrite/unsafe.ml
+++ src/compiler/rewrite/unsafe.ml
@@ -15,27 +15,60 @@
 (* safe/unsafe expressions and equations. *)
 (* A computation is safe when it is combinatorial, that is, it *)
 (* has no side effect, total and no state *)
+open Lident
 open Zelus
-open Ident
-open Deftypes
-open Aux
        
+(* the safe/unsafe sort must be added to the sorting mechanism during typing *)
+(* for the moment, we restrict safe functions to be the following ones *)
+let list_of_primitives =
+  ["+", 2;
+   "-", 2;
+   "~-", 1;
+   "-", 2;
+   "/", 2;
+   "*", 2;
+   "+.", 2;
+   "-.", 2;
+   "~-.", 1;
+   "-.", 2;
+   "/.", 2;
+   "*.", 2;
+   "sqrt", 1;
+   "sin", 1;
+   "cos", 1;
+   "abs_float", 1;
+   "abs", 1;
+   "not", 1;
+   "&&", 2;
+   "&", 2;
+   "or", 2;
+   "||", 2;
+   "mod", 2;
+   "=", 2;
+   "<", 2;
+   ">", 2;
+   "<=", 2;
+   ">=", 2]
+
+module Env = Map.Make(String)
+
+let env_of_primitives =
+  List.fold_left (fun acc (p, arity) -> Env.add p arity acc) Env.empty 
+    list_of_primitives
+
 (* An expression or equation is unsafe if it contains an unsafe operation. *)
-let expression funs acc e =
-  let { e_desc; e_info } as e, acc = Mapfold.expression funs acc e in
-  if acc then e, acc
-  else
-    let ty = Typinfo.get_type e_info in
-    match e_desc with
-    | Eapp { f; arg_list } ->
-       (* look if (f e1...en) is combinatorial *)
-       e, (not (Types.is_combinatorial (List.length arg_list) ty))
-    | _ -> e, acc
-
-let expression e =
-  let global_funs = Mapfold.default_global_funs in
-  let funs =
-    { Mapfold.defaults with expression; global_funs } in
-  let _, acc = Mapfold.expression_it funs false e in
-  acc
-
+let rec expression { e_desc } =
+  match e_desc with
+  | Econst _ | Econstr0 _ -> true
+  | Econstr1 { arg_list } | Etuple(arg_list) ->
+     List.for_all expression arg_list
+  | Eapp { f = { e_desc = Eglobal { lname = Modname { qual; id } } };
+           arg_list } ->
+     if qual = Misc.name_of_stdlib_module 
+     then try
+         let arity = Env.find id env_of_primitives in
+         (arity = List.length arg_list) && (List.for_all expression arg_list)
+       with
+         Not_found -> false
+     else false
+  | _ -> false
