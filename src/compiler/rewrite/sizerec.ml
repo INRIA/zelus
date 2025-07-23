eb1a769fb5839bce7667b2b2db3d8ee6c0115008
Marc Pouzet <Marc.Pouzet@ens.fr>
2025-07-23 05:10:55 +0200

    Update sizerec.ml

--- src/compiler/rewrite/sizerec.ml
+++ src/compiler/rewrite/sizerec.ml
@@ -48,7 +48,9 @@ let empty =
     env_of_sizefun = Env.empty; env_of_sizes = Env.empty }
 
 let add_sizefun funs ({ env_of_sizefun } as acc) ({ sf_id } as sizefun) =
-  { acc with env_of_sizefun  = Env.add sf_id sizefun env_of_sizefun  }
+  { acc with env_of_sizefun = Env.add sf_id sizefun env_of_sizefun  }
+
+let size env_of_size s = 42
 
 let equation funs acc ({ eq_desc; eq_loc } as eq) =
   match eq_desc with
@@ -63,7 +65,8 @@ let equation funs acc ({ eq_desc; eq_loc } as eq) =
      { eq with eq_desc = EQlet({ leq with l_eq = Aux.par eq_list }, eq_let) }, acc
   | _ -> raise Mapfold.Fallback
 
-let expression funs acc ({ e_desc; e_loc } as e) =
+let expression funs ({ memo; env_of_sizefun; env_of_sizes } as acc) 
+      ({ e_desc; e_loc } as e) =
   match e_desc with
   | Elet({ l_eq } as leq, e_let) ->
      let eq_list, acc = 
@@ -74,9 +77,17 @@ let expression funs acc ({ e_desc; e_loc } as e) =
      let eq_list, acc = Util.mapfold (Mapfold.equation_it funs) acc eq_list in
      let e_let, acc = Mapfold.expression_it funs acc e_let in
      { e with e_desc = Elet({ leq with l_eq = Aux.par eq_list }, e_let) }, acc
-  | Esizeapp { f; size_list } ->
+  | Esizeapp { f = { e_desc = Evar(f) }; size_list } ->
      (* [f <<s1,...>>] where the [s_i] are immediate values] *)
-        e, acc
+     begin try
+        let { sf_id; sf_id_list; sf_e; sf_loc; sf_env } as f =
+          Env.find f env_of_sizefun in
+        let v_list = List.map (size env_of_sizes) size_list in
+        let f_id = Memo.find (sf_id, v_list) memo in
+        { e with e_desc = Evar(f_id) }, acc
+       with
+         Not_found -> e, acc
+     end
   | _ -> raise Mapfold.Fallback
 
 let set_index funs acc n =
