3e723832190874f2bb5942ea5305b28f57d7d37b
Victor Miquel <victor.miquel@ens.psl.eu>
2026-07-08 18:17:45 +0200

    dune: rewrite dune
    Additional changes:
    - Move config.ml to src/config.ml
    
    Co-authored-by: Balthazar Patiachvili <balthazar.patiachvili@crans.org>

--- config.ml
+++
@@ -1,4 +0,0 @@
-let version = "2024"
-let subversion = "dev"
-let stdlib = "/Users/pouzet/article/paper/lustre/hybrid/zelus_git/github/zelus/_build/install/default/share/zelus"
-let date = "2026-07-7-8:20"
